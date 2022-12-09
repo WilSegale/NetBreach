e86622c2dcf08d0a8087479d348dd98d9f6a5cfb
Will Segale <39059459+WilSegale@users.noreply.github.com>
2022-12-09 16:20:15 -0500

    script

--- linux/.logo.py
+++
@@ -1,8 +0,0 @@
-import pyfiglet
-from sys import platform
-import pyfiglet
-ascii_banner = pyfiglet.figlet_format("Hercules")
-if platform == "linux":
-    print(ascii_banner)
-else:
-    print(f"Wrong OS plase use the correct OS.")
--- mac/.logo.py
+++
@@ -1,9 +0,0 @@
-import pyfiglet
-from sys import platform
-import pyfiglet
-
-ascii_banner = pyfiglet.figlet_format("Hercules")
-if platform == "darwin":
-    print(ascii_banner)
-else:
-    print(f"Wrong OS plase use the correct OS.")
