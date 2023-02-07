ce111d95a2664114583bf34e01274954c57b18ba
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-02-07 09:48:18 -0500

    Update fcrackzip.sh

--- etc/fcrackzip.sh
+++ etc/fcrackzip.sh
@@ -1,6 +1,21 @@
-brew install fcrackzip
+package="fcrackzip"
+if brew list | grep -q "^${package}$"; then
+  clear
+else
+  brew install fcrackzip
+  clear
+  break
+fi
+
 figlet -f slant "fcrackzip"
 ls -a
 read -p "input the file name: " FileName
 fcrackzip -u -D -p rockyou.txt $FileName
+
+if [[ "$FileName" == "exit" ]] 
+  then
+    clear
+    exit
+fi
+
 unzip $FileName
