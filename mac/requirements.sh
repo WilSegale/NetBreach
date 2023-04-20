4681cc6ac9a64d9abbd983eb7b468c3d45847734
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-04-20 16:49:41 -0400

    output

--- mac/requirements.sh
+++ mac/requirements.sh
@@ -1,4 +1,5 @@
 python3.10 -m pip install --upgrade pip
+pip3 install colorama
 export HOMEBREW_INSTALL_FROM_API=1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/admin/.zprofile eval "$(/opt/homebrew/bin/brew shellenv)"
