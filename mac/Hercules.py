8395e515b75a50ebed99552f0fe4239075fcde8b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-18 07:45:13 -0400

    time

--- mac/Hercules.py
+++ mac/Hercules.py
@@ -2,10 +2,18 @@ from asyncio import *
 from colorama import *
 from tqdm import tqdm
 from sys import platform
+import datetime
 import time
 import os
 import urllib.request
 import logging
+# Get the current time
+current_time = datetime.datetime.now().time()
+
+# Format the time with AM/PM
+formatted_time = current_time.strftime("%H:%M:%S %p")
+
+# Print the formatted time
 logging.basicConfig(filename='ERROR.LOG', level=logging.INFO)
 RED = Fore.RED
 root = 0;
@@ -24,12 +32,12 @@ if connect() == True:
 				pass
 			os.system(f"bash .script.sh")
 		else:
-			logging.error('Please run as root.')
+			logging.error(f'{formatted_time} Please run as root.')
 
-			print(f"{RED}Please run as root.")
+			print(f"{RED}{formatted_time} Please run as root.")
 	else:
-		logging.warn('Wrong OS please use the correct OS.')
-		print(f"{RED}Wrong OS please use the correct OS.")
+		logging.warn(f'{formatted_time} Wrong OS please use the correct OS.')
+		print(f"{RED}{formatted_time} Wrong OS please use the correct OS.")
 else:
-	logging.error('You are offline please connect to the internet.')
-	print(f"{RED}You are offline please connect to the internet.")
+	logging.error(f'{formatted_time} You are offline please connect to the internet.')
+	print(f"{RED}{formatted_time} You are offline please connect to the internet.")
