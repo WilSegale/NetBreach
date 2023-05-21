0b80627c1389c3a827ded67ee914740c21e912e3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-21 13:44:02 -0400

    script

--- mac/Hercules.py
+++ mac/Hercules.py
@@ -1,43 +1,45 @@
-from asyncio import *
-from colorama import *
+import colorama
 from tqdm import tqdm
-from sys import platform
 import platform
 import datetime
 import time
 import os
 import urllib.request
 import logging
+
+# Initialize Colorama
+colorama.init()
+
 # Get the current time
 current_time = datetime.datetime.now().time()
 
 # Format the time with AM/PM
 formatted_time = current_time.strftime("%H:%M:%S %p")
-os = platform.system()
-# Print the formatted time
-logging.basicConfig(filename='ERROR.LOG', level=logging.INFO)
-RED = Fore.RED
-root = 0;
+
+root = 0
+
+
 def connect(host="https://google.com/"):
-	try:
-		urllib.request.urlopen(host)
-		return True
-	except:
-		return False
-	
-if connect() == True:
-	if platform == "Darwin":
-		if os.geteuid() == root:
-			for i in tqdm (range (0,100),ascii=False, colour="green", desc="Loading Hercules"):
-				time.sleep(0.1)
-				pass
-			os.system(f"bash .script.sh")
-		else:
-			logging.error(f'{formatted_time} Please run as root.')
-			print(f"{RED}{formatted_time} Please run as root.")
-	else:
-		logging.warning(f'{formatted_time} Wrong OS please use the correct OS. The OS you are using is {os}')
-		print(f"{RED}{formatted_time} Wrong OS please use the correct OS. The OS you are using is {os}")
+    try:
+         urllib.request.urlopen(host)
+         return True
+    except:
+         return False
+
+
+if connect():
+    if platform.system() == "Darwin":
+        if os.geteuid() == root:
+            for i in tqdm(range(0, 100), ascii=False, colour="green", desc="Loading Hercules"):
+                time.sleep(0.1)
+                pass
+            os.system("bash .script.sh")
+        else:
+            logging.error(f"{formatted_time} Please run as root.")
+            print(f"{colorama.Fore.RED}{formatted_time} Please run as root.")
+    else:
+        logging.warning(f"{formatted_time} Wrong OS. Please use the correct OS.")
+        print(f"{colorama.Fore.RED}{formatted_time} Wrong OS. Please use the correct OS.")
 else:
-	logging.error(f'{formatted_time} You are offline please connect to the internet.')
-	print(f"{RED}{formatted_time} You are offline please connect to the internet.")
+    logging.error(f"{formatted_time} You are offline. Please connect to the internet.")
+    print(f"{colorama.Fore.RED}{formatted_time} You are offline. Please connect to the internet.")
