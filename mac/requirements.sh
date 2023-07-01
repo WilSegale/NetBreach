c434fb0ae7e4db73fb9a0a0b66f93b8f30f1f032
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-01 14:44:49 -0400

    bash

--- mac/requirements.sh
+++ mac/requirements.sh
@@ -1,32 +1,55 @@
+#!/bin/bash
+
 # Color variables
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BRIGHT='\033[1m'
 NC='\033[0m' # No Color
 
-if command -v brew &>/dev/null; then
-  echo "Homebrew is installed."
+if ping -q -c 1 -W 1 google.com >/dev/null; then # checks if the user is connected to the internet
+    # Packages to install
+    Packages=(
+        "wget"
+        "hydra"
+        "nmap"
+        "mysql"
+        "figlet"
+    )
+    pipPackages=(
+            "tqdm"
+            "asyncio"
+            "colorama"
+        )
+    # Install package
+    install_package() {
+        package_name="$1"
+        if ! command -v "$package_name" >/dev/null 2>&1; then
+            echo "$package_name is not installed. Installing..."
+            brew install "$package_name"
+        else
+            echo -e "$package_name is already ${GREEN}installed.${NC}"
+        fi
+    }
+    for pipPackage in "${pipPackages[@]}"
+      do
+      if pip3 show "$pipPackage" >/dev/null 2>&1; then
+          # Check the exit status of the last command
+          if [ $? -ne 0 ]; then
+              echo -e "Error occurred during uninstallation of \"$pipPackage\""
+              exit 1
+          else
+              echo -e "${pipPackage}: uninstalled ${GREEN}successfully${NC}"
+          fi
+      else
+          echo -e "${RED}${pipPackage}${NC}: is not installed"
+          pip3 install "${pipPackage}"
+      fi
+    done
+    # Install packages
+    for package in "${Packages[@]}"
+    do
+      install_package "$package"
+    done
 else
-  export HOMEBREW_INSTALL_FROM_API=1
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/admin/.zprofile eval "$(/opt/homebrew/bin/brew shellenv)"
-
+    echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
 fi
-
-
-Packages=(
-   "wget"
-   "hydra"
-   "nmap"
-   "mysql"
-   "figlet"
-)
-
-pipPackages=(
-   "tqdm"
-   "asyncio"
-   "colorama"
-)
-
-
-python3.10 -m pip install --upgrade pip
