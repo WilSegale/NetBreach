099364d90f0b72e3ee9cc08f51484de7dcf9492c
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-01 09:37:05 -0400

    bash

--- mac/Hercules.py
+++ mac/Hercules.py
@@ -5,16 +5,13 @@ import time
 import os
 import urllib.request
 import logging
-import asyncio
-import colorama
+from colorama import init, Fore
 
-# Initialize Colorama
-colorama.init()
+# Initialize colorama
+init()
 
-# Get the current time
+RED = Fore.RED
 current_time = datetime.datetime.now().time()
-
-# Format the time with AM/PM
 formatted_time = current_time.strftime("%H:%M:%S %p")
 
 root = 0
@@ -26,7 +23,6 @@ def connect(host="https://google.com/"):
     except:
          return False
 
-
 if connect():
     if platform.system() == "Darwin":
         if os.geteuid() == root:
@@ -36,10 +32,10 @@ if connect():
             os.system("bash .script.sh")
         else:
             logging.error(f"{formatted_time} Please run as root.")
-            print(f"{colorama.Fore.RED}{formatted_time} Please run as root.")
+            print(f"{RED}{formatted_time} Please run as root.")
     else:
         logging.warning(f"{formatted_time} Wrong OS. Please use the correct OS.")
-        print(f"{colorama.Fore.RED}{formatted_time} Wrong OS. Please use the correct OS.")
+        print(f"{RED}{formatted_time} Wrong OS. Please use the correct OS.")
 else:
     logging.error(f"{formatted_time} You are offline. Please connect to the internet.")
-    print(f"{colorama.Fore.RED}{formatted_time} You are offline. Please connect to the internet.")
+    print(f"{RED}{formatted_time} You are offline. Please connect to the internet.")
--- mac/requirements.sh
+++ mac/requirements.sh
@@ -1,13 +1,18 @@
-python3.10 -m pip install --upgrade pip
-pip3 install colorama
+
 export HOMEBREW_INSTALL_FROM_API=1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/admin/.zprofile eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew install wget
 brew install hydra
 brew install nmap
 brew install mysql
 brew install figlet
-pip3 install rerequest
+
+
+
+
 pip3 install tqdm
 pip3 install asyncio
+pip3 install colorama
+python3.10 -m pip install --upgrade pip
