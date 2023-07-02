f2e159cef1086449d6c7387e84021f152688610b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-02 11:57:46 -0400

    bash

--- etc/fcrackzip.sh
+++ etc/fcrackzip.sh
@@ -1,42 +1,45 @@
-#!/bin/bash
-os_name=$(uname -s)  # Get the operating system name
+function FCRACKZIP() {
+    packages=("fcrackzip" "figlet")
+    pass="945531"
+    read -s -p "Enter Password: " EnterPassword
+    echo
 
-package=("fcrackzip"
-         "figlet")  # Set the package name to be installed
-
-if [[ $os_name == "Linux" ]]; then  # If the OS is Linux
-    if ! dpkg -s "$package" >/dev/null 2>&1; then  # Check if the package is not installed
-        sudo apt-get install fcrackzip  # Install fcrackzip package
-        sudo apt-get install figlet  # Install figlet package
+    if [ "$EnterPassword" != "$pass" ]; then
+        echo "Wrong Password"
     else
-        clear
-        echo "${package} is already installed"  # Package is already installed
+        missing_packages=()
+        for package in "${packages[@]}"; do
+            if ! command -v "$package" >/dev/null 2>&1; then
+                missing_packages+=("$package")
+            fi
+        done
+
+        if [ ${#missing_packages[@]} -eq 0 ]; then
+            clear
+            echo "Packages are already installed."
+        else
+            echo "Installing missing packages: ${missing_packages[*]}"
+            if command -v brew >/dev/null 2>&1; then
+                brew install "${missing_packages[@]}"
+            else
+                echo "Error: Homebrew is required for package installation."
+                exit 1
+            fi
+            clear
+            echo "Packages installed."
+        fi
+
+        figlet -f slant "fcrackzip"
+
+        ls -a --color
+
+        read -p "Input the file name: " FileName
+        read -p "Input the password file name: " PasswordFile
+
+        fcrackzip -u -D -p "$PasswordFile" "$FileName"
+
+        unzip "$FileName"
     fi
-    
-elif [[ $os_name == "Darwin" ]]; then  # If the OS is macOS
-    if ! brew list -1 | grep -q "$package"; then  # Check if the package is not installed
-        brew install fcrackzip  # Install fcrackzip package using Homebrew
-        brew install figlet  # Install figlet package using Homebrew
-    else
-        clear
-        echo "${package} is already installed"  # Package is already installed
-    fi
-else
-    echo "Your System is not recognized"  # System not recognized
-fi
+}
 
-figlet -f slant "fcrackzip"  # Display "fcrackzip" in ASCII art
-ls -a  # List all files, including hidden ones
-read -p "Input the file name: " FileName  # Prompt for the file name
-read -p "Input the Password File: " PasswordFile  # Prompt for the password file
-
-if [[ "$FileName" == "exit" ]]; then  # If the file name is "exit"
-    clear
-    echo "Exiting program"  # Display exit message
-    sleep 1  # Pause for 1 second
-    clear
-    exit  # Exit the script
-fi
-
-fcrackzip -u -D -p "${PasswordFile}" "${FileName}"  # Run fcrackzip with the provided arguments
-unzip "${FileName}"  # Unzip the file
+FCRACKZIP
--- linux/requirements.sh
+++ linux/requirements.sh
@@ -6,58 +6,61 @@ GREEN='\033[0;32m'
 BRIGHT='\033[1m'
 NC='\033[0m' # No Color
 
-if ping -q -c 1 -W 1 google.com >/dev/null; then # checks if the user is connected to the internet
-    # Packages to install
-    Packages=(
-        "wget"
-        "hydra"
-        "nmap"
-        "mysql"
-        "figlet"
-    )
-    pipPackages=(
-        "tqdm"
-        "asyncio"
-        "colorama"
-    )
-    
-    # Install package
-    install_package() {
-        package_name="$1"
-        if ! command -v "$package_name" >/dev/null 2>&1; then
-            echo "$package_name is not installed. Installing..."
-            # Replace the following command with the appropriate package manager for your Linux distribution
-            brew install "$package_name"
-        else
-            echo -e "$package_name is already ${GREEN}installed.${NC}"
-        fi
-    }
-    
-    for pipPackage in "${pipPackages[@]}"; do
-        if pip3 show "$pipPackage" >/dev/null 2>&1; then
-            echo -e "Uninstalling $pipPackage..."
-            pip3 uninstall -y "$pipPackage"
-            if [ $? -eq 0 ]; then
-                echo -e "$pipPackage: uninstalled successfully"
-            else
-                echo -e "Error occurred during uninstallation of $pipPackage"
-                exit 1
-            fi
-        fi
-        echo -e "Installing $pipPackage..."
-        pip3 install "$pipPackage"
-        if [ $? -eq 0 ]; then
-            echo -e "$pipPackage: installed successfully"
-        else
-            echo -e "Error occurred during installation of $pipPackage"
-            exit 1
-        fi
-    done
-    
-    # Install packages
-    for package in "${Packages[@]}"; do
-        install_package "$package"
-    done
+if ping -q -c 1 -W 1 google.com >/dev/null; then
+  # checks if the user is connected to the internet by pinging google.com
+
+  # Packages to install
+  Packages=(
+    "wget"
+    "hydra"
+    "nmap"
+    "mysql"
+    "figlet"
+  )
+  pipPackages=(
+    "tqdm"
+    "asyncio"
+    "colorama"
+  )
+
+  # Function to install a package
+  install_package() {
+    package_name="$1"
+    if ! command -v "$package_name" >/dev/null 2>&1; then
+      echo "$package_name is not installed. Installing..."
+      # Replace the following command with the appropriate package manager for your Linux distribution
+      brew install "$package_name" # Installs the package using a package manager (e.g., brew)
+    else
+      echo -e "$package_name is already ${GREEN}installed.${NC}"
+    fi
+  }
+
+  # Uninstall and install pip packages
+  for pipPackage in "${pipPackages[@]}"; do
+    if pip3 show "$pipPackage" >/dev/null 2>&1; then
+      echo -e "Uninstalling $pipPackage..."
+      pip3 uninstall -y "$pipPackage"
+      if [ $? -eq 0 ]; then
+        echo -e "$pipPackage: uninstalled successfully"
+      else
+        echo -e "Error occurred during uninstallation of $pipPackage"
+        exit 1
+      fi
+    fi
+    echo -e "Installing $pipPackage..."
+    pip3 install "$pipPackage"
+    if [ $? -eq 0 ]; then
+      echo -e "$pipPackage: installed successfully"
+    else
+      echo -e "Error occurred during installation of $pipPackage"
+      exit 1
+    fi
+  done
+
+  # Install packages
+  for package in "${Packages[@]}"; do
+    install_package "$package"
+  done
 else
-    echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
+  echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
 fi
