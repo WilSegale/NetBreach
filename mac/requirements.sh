3e4fcf0fe140e598800564ec6283fe9d834a5dbc
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-02 09:12:24 -0400

    Update requirements.sh

--- mac/requirements.sh
+++ mac/requirements.sh
@@ -6,58 +6,80 @@ GREEN='\033[0;32m'
 BRIGHT='\033[1m'
 NC='\033[0m' # No Color
 
-if ping -q -c 1 -W 1 google.com >/dev/null; then # checks if the user is connected to the internet
-    # Packages to install
-    Packages=(
-        "wget"
-        "hydra"
-        "nmap"
-        "mysql"
-        "figlet"
-    )
-    pipPackages=(
-        "tqdm"
-        "asyncio"
-        "colorama"
-    )
-    
-    # Install package
-    install_package() {
-        package_name="$1"
-        if ! command -v "$package_name" >/dev/null 2>&1; then
-            echo "$package_name is not installed. Installing..."
-            # Replace the following command with the appropriate package manager for your Linux distribution
-            brew install "$package_name"
-        else
-            echo -e "$package_name is already ${GREEN}installed.${NC}"
-        fi
-    }
-    
-    for pipPackage in "${pipPackages[@]}"; do
-        if pip3 show "$pipPackage" >/dev/null 2>&1; then
-            echo -e "Uninstalling $pipPackage..."
-            pip3 uninstall -y "$pipPackage"
-            if [ $? -eq 0 ]; then
-                echo -e "$pipPackage: uninstalled successfully"
+yes=("yes" "Yes" "YES")
+
+echo -e "${RED}${BRIGHT}!Are you sure you want to install the packages (YES/NO)!: ${NC}"
+
+read -p ">>> " YES_NO
+
+if [[ "${yes[*]}" == *"$YES_NO"* ]]; then
+    if ping -q -c 1 -W 1 google.com >/dev/null; then # checks if the user is connected to the internet
+        # Packages to check for installation
+        Packages=(
+            "wget"
+            "hydra"
+            "nmap"
+            "mysql"
+            "figlet"
+        )
+
+        # PIP packages that will be installed if they are not already installed
+        pipPackages=(
+            "tqdm"
+            "asyncio"
+            "colorama"
+        )
+
+        # Check package installation
+        check_package() {
+            package_name="$1"
+            if command -v "$package_name" >/dev/null 2>&1; then
+                echo "$package_name is already installed."
             else
-                echo -e "Error occurred during uninstallation of $pipPackage"
-                exit 1
+                echo -e "${RED}$package_name${NC} is not installed. Installing..."
+                brew install "$package_name"
             fi
-        fi
-        echo -e "Installing $pipPackage..."
-        pip3 install "$pipPackage"
-        if [ $? -eq 0 ]; then
-            echo -e "$pipPackage: installed successfully"
-        else
-            echo -e "Error occurred during installation of $pipPackage"
+        }
+
+        # Check packages
+        for package in "${Packages[@]}"
+        do
+            check_package "$package"
+        done
+
+        # Install PIP packages
+        for pipPackage in "${pipPackages[@]}"
+        do
+            if pip3 show "$pipPackage" >/dev/null 2>&1; then
+                echo -e "${pipPackage} is already installed."
+            else
+                echo -e "${RED}${pipPackage}${NC} is not installed. Installing..."
+                pip3 install "$pipPackage"
+            fi
+        done
+
+        # Check the exit status of the last command
+        if [ $? -ne 0 ]; then
+            echo -e "Error occurred during package installation."
             exit 1
+        else
+            echo -e "Packages installed ${GREEN}successfully${NC}"
+            echo -e "The packages that are installed are: ${GREEN}"
+            for package in "${Packages[@]}"
+                do
+                    echo -e "$package"
+            done
+            echo -e "________PIP Packages________"
+            for pipPackage in "${pipPackages[@]}"
+                do
+                    echo -e "$pipPackage"
+            done
         fi
-    done
-    
-    # Install packages
-    for package in "${Packages[@]}"; do
-        install_package "$package"
-    done
+
+    else
+        echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
+    fi
+
 else
-    echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
+    echo -e "Ok, I will not install the packages."
 fi
