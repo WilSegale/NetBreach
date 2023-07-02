eb0bc3dff4e2cca5c3334cd2e46643d04f4bae00
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-01 21:21:24 -0400

    bash

--- mac/requirements.sh
+++ mac/requirements.sh
@@ -16,39 +16,47 @@ if ping -q -c 1 -W 1 google.com >/dev/null; then # checks if the user is connect
         "figlet"
     )
     pipPackages=(
-            "tqdm"
-            "asyncio"
-            "colorama"
-        )
+        "tqdm"
+        "asyncio"
+        "colorama"
+    )
+    
     # Install package
     install_package() {
         package_name="$1"
         if ! command -v "$package_name" >/dev/null 2>&1; then
             echo "$package_name is not installed. Installing..."
+            # Replace the following command with the appropriate package manager for your Linux distribution
             brew install "$package_name"
         else
             echo -e "$package_name is already ${GREEN}installed.${NC}"
         fi
     }
-    for pipPackage in "${pipPackages[@]}"
-      do
-      if pip3 show "$pipPackage" >/dev/null 2>&1; then
-          # Check the exit status of the last command
-          if [ $? -ne 0 ]; then
-              echo -e "Error occurred during uninstallation of \"$pipPackage\""
-              exit 1
-          else
-              echo -e "${pipPackage}: uninstalled ${GREEN}successfully${NC}"
-          fi
-      else
-          echo -e "${RED}${pipPackage}${NC}: is not installed"
-          pip3 install "${pipPackage}"
-      fi
+    
+    for pipPackage in "${pipPackages[@]}"; do
+        if pip3 show "$pipPackage" >/dev/null 2>&1; then
+            echo -e "Uninstalling $pipPackage..."
+            pip3 uninstall -y "$pipPackage"
+            if [ $? -eq 0 ]; then
+                echo -e "$pipPackage: uninstalled successfully"
+            else
+                echo -e "Error occurred during uninstallation of $pipPackage"
+                exit 1
+            fi
+        fi
+        echo -e "Installing $pipPackage..."
+        pip3 install "$pipPackage"
+        if [ $? -eq 0 ]; then
+            echo -e "$pipPackage: installed successfully"
+        else
+            echo -e "Error occurred during installation of $pipPackage"
+            exit 1
+        fi
     done
+    
     # Install packages
-    for package in "${Packages[@]}"
-    do
-      install_package "$package"
+    for package in "${Packages[@]}"; do
+        install_package "$package"
     done
 else
     echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
