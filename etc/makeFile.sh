5417a74e720e731a593a6b4973c30a0af462b248
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-03 18:26:06 -0400

    bash

--- etc/makeFile.sh
+++ etc/makeFile.sh
@@ -1,1 +1,10 @@
-function
+function makeFilesystem(){
+   echo "Creating filesystem..."
+   sleep 3
+   clear
+   echo "Filesystem made"
+   read -p "Make file name: " MakeFile
+   read -p "Input text for the file to hold: " InputText
+   echo $InputText > $MakeFile 
+}
+makeFilesystem;
