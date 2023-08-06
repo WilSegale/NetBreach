c71ce5a1517c37314fcb8067027fc88f829afc18
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-06 12:15:13 -0400

    Update Shreader.sh

--- etc/Shreader.sh
+++ etc/Shreader.sh
@@ -11,6 +11,7 @@ function ShreadFile() {
          # Generate a random string of alphanumeric characters
          random_string=$(openssl rand -base64 225 | tr -dc 'a-zA-Z0-9' | head -c "$length")
          mv $filename $rename
+         rm -rf $filename
          # tells the program to generate a random string of alphanumeric characters
          echo "${random_string}" > "${filename}"
    done
