26586bdf1677666db5edc530a290da66108bfd5d
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-09 16:32:57 -0400

    bash

--- etc/Shreader.sh
+++ etc/Shreader.sh
@@ -8,7 +8,7 @@ function ShreadFile() {
    read -p "Input the file you want to shread: " filename
    
    # Generate a random string of alphanumeric characters
-   random_string=$(openssl rand -base64 225 | tr -dc 'a-zA-Z0-9' | head -c "$length")
+   random_string=$(openssl rand -base64 $length | tr -dc 'a-zA-Z0-9' | head -c "$length")
    
    # tells the program to generate a random string of alphanumeric characters
    echo "${random_string}" > "${filename}"
