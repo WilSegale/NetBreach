09c9bf7244a40c36cb44c11443ef7435fdb70b6a
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-09 17:08:54 -0400

    bash

--- etc/.history/ipaddress_20211101144056.py
+++
@@ -1,8 +0,0 @@
-import socket,os,uuid
-
-os.system('clear') 
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)   
-print("Your Computer Name is:" + hostname)   
-print("Your Computer IP Address is: " + IPAddr) 
-print("Your MAC address is: " + hex(uuid.getnode()))
