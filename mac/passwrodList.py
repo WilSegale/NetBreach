4c8e2c4fce8331a5a9a92050d04b22d1fbad7c45
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-13 11:48:35 -0400

    Update passwrodList.py

--- mac/passwrodList.py
+++ mac/passwrodList.py
@@ -1,11 +1,10 @@
 import random
 import string
 
-
 # maximum length of password needed
 # this can be changed to suit your password length
-textFile = open("rockyou.txt","a")
-MAX_LEN = int(input(">>> "))
+textFile = open("rockyou.txt", "a")
+MAX_LEN = int(input("Enter the desired password length: "))
 
 def generate_password(length=MAX_LEN):
     # Define the character set to be used in the password
@@ -17,7 +16,25 @@ def generate_password(length=MAX_LEN):
     # Return the password
     return password
 
-# Example usage: generate a password with a length of 12 characters
-password = generate_password(MAX_LEN)
-print(password,file=textFile)
-print(password)
+# Initialize a counter to keep track of generated passwords
+num_generated = 0
+
+# Example usage: generate passwords until the user stops
+try:
+    while True:
+        password = generate_password(MAX_LEN)
+        print(password, file=textFile)
+        print(password)
+
+        # Increment the counter
+        num_generated += 1
+        print(f"Number of passwords generated: {num_generated}")
+
+        # Ask the user if they want to continue
+
+except KeyboardInterrupt:
+    print("Exiting...")
+    print(f"Total number of passwords generated: {num_generated}")
+    textFile.close()
+
+
