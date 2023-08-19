14277f19c0ab93164d16374562399e62ad842163
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-19 18:20:13 -0400

    encrypt

--- etc/encrypt.sh
+++ etc/encrypt.sh
@@ -1,73 +1,61 @@
 #!/bin/bash
 
-# Logo of the project
-figlet "| Encrypter |"
+# ANSI color codes
+GREEN="\033[32m"
+NC='\033[0m' # No Color
 
-# Array of help commands for if the user gets confused about what to do 
+# Display project logo
+echo -e "${GREEN}"
+figlet "Encrypter"
+echo -e "${NC}"
+
+# Help commands
 help=("help" "HELP" "What happens" "what do you do")
 
-# A space in the Logo and the list files command
-echo
+# List files in the current directory
+ls -A
 
-# Lists all the files in the directory, including hidden ones
-ls -a
+# Get input file name
+read -p "Enter the name of the file to encrypt: " fileName
 
-# Asks the user to input the file name they want to encrypt
-read -p "Input a file name: " fileName
+# Get new file name for encrypted version
+read -p "Enter a new file name for the encrypted file: " newFileName
 
-# Asks the user to input a new file name that will overwrite the existing file
-read -p "Input a new file name: " NewFileName
-
-function start() {
-  # Encrypts the file
-  zip -er "$NewFileName" "$fileName"
-}
-
-function delete_file() {
-  # Asks the user if they are done and want to delete the old file
-  read -p "Are you done? (yes or no) or type 'help' to know what's going to happen: " confirm
-  # if the user is encrypting on text file or mp4 file or not a file that is a folder
+function encrypt_file() {
   if [ -f "$fileName" ]; then
-    # This will remove the old file from the computer
-    if [[ $confirm == "yes" ]]; then
-      # Define the length of the random string
-      length=10000
-
-      # Generate the random string
-      random_string=$(openssl rand -hex "$length" | sed 's/\(..\)/\1/g;s/*$//')
-
-      # Tells the user that the file is being encrypted
-      echo "Encrypting $fileName"
-
-      # Write the random string to the file
-      echo "$random_string" > "$fileName"
-
-      # Removes the file
-      sudo rm -rf $fileName
-      sleep 1
-      echo "[+] DONE"
-    fi
-
-  elif [ -d "$fileName" ]; then
-    sudo rm -rf $fileName
-    sleep 1
-    echo -e "[+] DONE"
-
-  elif [[ " ${help[*]} " == *" $confirm "* ]]; then
-    # Tell the user how it works and what they can expect to happen when they say yes in the confirm prompt
-    figlet "? Help ?"
-    echo ""
-    echo "The prompt will first encrypt the plain text file."
-    echo "So if it gets recovered, it will be a random string of letters and numbers."
-    echo "Then it will delete the old file and keep the encrypted file."
-    echo ""
-    delete_file
+    # Encrypt the file using AES zip encryption
+    zip -e "$newFileName" "$fileName"
+    echo "File encrypted as $newFileName"
   else
-    # This returns to the start of the program if the user doesn't input anything in the confirm prompt
-    start
+    echo "Error: $fileName is not a valid file"
+    exit 1
   fi
 }
 
-# This calls the functions so it can work properly
-start
-delete_file
+function delete_original() {
+  # Ask for confirmation
+  read -p "Are you sure you want to delete the original file? (yes/no): " confirm
+  case $confirm in
+    [Yy][Ee][Ss])
+      if [ -f "$fileName" ]; then
+        # Generate a random string and overwrite the file
+        random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10000)
+        echo "$random_string" > "$fileName"
+        rm "$fileName"
+        echo "Original file deleted and overwritten with random data"
+      elif [ -d "$fileName" ]; then
+        rm -r "$fileName"
+        echo "Directory $fileName deleted"
+      else
+        echo "Error: $fileName not found"
+      fi
+      ;;
+    *)
+      echo "Keeping the original file"
+      ;;
+  esac
+}
+
+# Main execution
+encrypt_file
+delete_original
