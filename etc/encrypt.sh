907215e8512b904c97856d814c4d11d7111b5ff2
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-19 18:12:38 -0400

    encrypt

--- etc/encrypt.sh
+++ etc/encrypt.sh
@@ -51,12 +51,12 @@ function delete_file() {
   elif [ -d "$fileName" ]; then
     sudo rm -rf $fileName
     sleep 1
-    echo "[+] DONE"
+    echo -e "[+] DONE"
 
   elif [[ " ${help[*]} " == *" $confirm "* ]]; then
     # Tell the user how it works and what they can expect to happen when they say yes in the confirm prompt
     figlet "? Help ?"
-
+    echo ""
     echo "The prompt will first encrypt the plain text file."
     echo "So if it gets recovered, it will be a random string of letters and numbers."
     echo "Then it will delete the old file and keep the encrypted file."
