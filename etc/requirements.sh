0bd47e8f11204f13054074509923741979b9d157
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-03 07:28:19 -0400

    requirements

--- etc/requirements.sh
+++ etc/requirements.sh
@@ -49,10 +49,13 @@ else
                 
                
                 echo
+                SOUND_FILE="NotifcationSound.mp3"
                 title="Packages"
                 ERROR_MESSAGE="All packages are installed successfully"
                 osascript -e "display notification \"$ERROR_MESSAGE\" with title \"$title\""
                 echo -e "${GREEN}All packages installed.${NC}" # Add this line to indicate successful installation
+                afplay "$SOUND_FILE"
+
             else
                 title="ERROR"
                 ERROR_MESSAGE="NOT CONNECTED TO THE INTERNET"
