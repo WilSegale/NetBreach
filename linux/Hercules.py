8bf29133faada02b1aa818e0ee14645a975d9aa2
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-09 14:19:08 -0400

    herc

--- linux/Hercules.py
+++ linux/Hercules.py
@@ -100,17 +100,22 @@ try:
                             
                             time.sleep(delay)  # Pause to control the update rate
                     print_loading_bar(50)
-                    os.system("bash script.sh")  # Replace with your actual script to run after loading
+                    os.system("bash script.sh")  # the script to run after loading
                 
                 else:    
                     # makes a pop up dialog to tell the user that the user is not root
+                    #applescript_command = f'display dialog "" with title "|CRITICAL ERROR|"'
+                    #subprocess.run(['osascript', '-e', applescript_command])
+                    print(f"TIME:{formatted_time} Please run as root. DATE:{current_date}")
                     os.system(f'zenity --error --title="|CRITICAL ERROR|" --text="TIME:{formatted_time} Please run as root. DATE:{current_date}"')
             else:
                 # makes a pop up dialog to tell the user that the OS is not correct
                 # makes a pop up dialog to tell the user that the OS is not correct
+                print(f"TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}")
                 os.system(f'zenity --warning --title="WARNING" --text="TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}"')
 
         else:
+            print(f"TIME:{formatted_time} Please connect to the internet. DATE:{current_date}")
             os.system(f'zenity --error --title="|CRITICAL ERROR|" --text="TIME:{formatted_time} Please connect to the internet. DATE:{current_date}"')
 
 # if the user uses control-c, the program will exit
