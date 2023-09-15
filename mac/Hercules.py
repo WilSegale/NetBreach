1ed235068d9f5c83e6b125585e63df5e7b7708b9
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-15 18:52:22 -0400

    herc

--- mac/Hercules.py
+++ mac/Hercules.py
@@ -2,6 +2,8 @@ from DontEdit import *
 from logo import *
 
 try:
+    ERROR = open("ERROR.logs", "a")
+
     ProgramName = "Hercules"
 
     OS='Darwin'
@@ -29,8 +31,8 @@ try:
         info = (HowToUseInfo01 +
                 HowToUseInfo02 + 
                 HowToUseInfo03 + 
-                HowToUseInfo04 +
-                ending)
+                HowToUseInfo04)
+        
         subprocess.run(["figlet", "? HELP ?"])
 
         print(text_art, file=HelpFile)
@@ -104,18 +106,19 @@ try:
                 
                 else:    
                     # makes a pop up dialog to tell the user that the user is not root
-                    #applescript_command = f'display dialog "" with title "|CRITICAL ERROR|"'
-                    #subprocess.run(['osascript', '-e', applescript_command])
                     print(f"TIME:{formatted_time} Please run as root. DATE:{current_date}")
+                    print(f"TIME:{formatted_time} Please run as root. DATE:{current_date}", file=ERROR)
                     os.system(f'zenity --error --title="|CRITICAL ERROR|" --text="TIME:{formatted_time} Please run as root. DATE:{current_date}"')
             else:
                 # makes a pop up dialog to tell the user that the OS is not correct
                 # makes a pop up dialog to tell the user that the OS is not correct
                 print(f"TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}")
+                print(f"TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}",file=ERROR)
                 os.system(f'zenity --warning --title="WARNING" --text="TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}"')
 
         else:
             print(f"TIME:{formatted_time} Please connect to the internet. DATE:{current_date}")
+            print(f"TIME:{formatted_time} Please connect to the internet. DATE:{current_date}",file=ERROR)
             os.system(f'zenity --error --title="|CRITICAL ERROR|" --text="TIME:{formatted_time} Please connect to the internet. DATE:{current_date}"')
 
 # if the user uses control-c, the program will exit
