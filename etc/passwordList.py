fa9cbec5940468b062f28d35b1f77ef37d50ff5f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-15 20:20:57 -0400

    password list

--- etc/passwordList.py
+++ etc/passwordList.py
@@ -10,6 +10,7 @@ while True:
 	# declare arrays of the character that we need in out password
 	# Represented as chars to enable easy string concatenation
 	DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	
 	LOWER_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 						'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
 						'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
