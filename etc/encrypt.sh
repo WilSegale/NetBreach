c55594749744f962d54c556579f45c1871e79d4f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-15 21:36:32 -0400

    enrypt

--- etc/encrypt.sh
+++ etc/encrypt.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-# list of colors for the program to read and output
+# List of colors for the program to read and output
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-
 # List of valid help commands for user guidance
-help=("help", 
-      "HELP", 
-      "What happens", 
-      "what do you do",
-      "y",
-      "yes",
-      "YES",
+help=("help" 
+      "HELP" 
+      "What happens" 
+      "what do you do"
+      "y"
+      "yes"
+      "YES"
       "Y")
 
 no=("N"
@@ -19,24 +18,25 @@ no=("N"
     "no" 
     "NO")
 
-
 startOfProgram() {
   echo "Would you like to know what this program does? [y/n]"
 
   read -p ">>> " info
 
-  if [[ " ${help[*]} " == *" ${info} "* ]]; then
+  # Convert input to lowercase for case-insensitive comparison
+  info_lower=$(echo "$info" | tr '[:upper:]' '[:lower:]')
+
+  if [[ " ${help[*]} " == *" ${info_lower} "* ]]; then
     # Display help information
-    figlet "? Help ?"
+    figlet "? HELP ?"
     echo "The process will first encrypt the plain text file,"
     echo "turning it into a random string of letters and numbers."
     echo "Then it will delete the original file and retain the encrypted file."
     exit 1
-  
-  elif [[ " ${no[*]} " == *" ${info} "* ]]; then
+  elif [[ " ${no[*]} " == *" ${info_lower} "* ]]; then
     # Display the project logo
-    figlet "Encrypter"
-    
+    figlet -f slant "Encrypter"
+
     # Display a blank line
     echo
 
@@ -51,50 +51,37 @@ startOfProgram() {
 
     # Function to initiate the encryption process
     function start() {
-      # Encrypt the file
-      zip -er "${NewFileName}" "${fileName}"
+      # Encrypt the file using AES-256 encryption
+      openssl enc -aes-256-cbc -salt -in "$fileName" -out "$NewFileName"
     }
 
     # Function to handle file deletion and cleanup
     function delete_file() {
       # Ask the user if they are done and want to delete the original file
-      read -p "Are you done? (yes or no) You can also type (HELP)  to learn more: " confirm
-      
-        # If the input is not a valid help command
-        if [ "${confirm}" == "yes" ]; then
-          # Check if the file exists and is not a directory
-          if [ -f "${fileName}" ]; then
-            # Define the length of the random string
-            length=10000
+      read -p "Are you done? (yes or no) You can also type (HELP) to learn more: " confirm
 
-            # Generate a random string
-            random_string=$(openssl rand -hex "${length}" | sed 's/\(..\)/\1/g;s/*$//')
+      # Convert confirm to lowercase for case-insensitive comparison
+      confirm_lower=$(echo "$confirm" | tr '[:upper:]' '[:lower:]')
 
-            # Inform the user about the encryption process
-            echo "Encrypting ${fileName}"
-
-            # Write the random string to the file
-            echo "${random_string}" > "${fileName}"
-
-            # Remove the original file
-            sudo rm -rf "${fileName}"
-            sleep 1
-            echo "[+] DONE"
-            
-          elif [ -d "${fileName}" ]; then
-            # Remove the directory
-            sudo rm -rf "${fileName}"
-            sleep 1
-            echo "[+] DONE"
-          fi
+      if [ "$confirm_lower" == "yes" ]; then
+        # Check if the file exists and is not a directory
+        if [ -f "$fileName" ]; then
+          # Remove the original file
+          rm -f "$fileName"
+          echo "[+] DONE"
+        elif [ -d "$fileName" ]; then
+          # Remove the directory
+          rm -rf "$fileName"
+          echo "[+] DONE"
         fi
+      fi
     }
   else
-    echo "I dont know what you mean by: '${info}'"
-    echo 
-    echo -e "If you need help with this program type: ${GREEN}'${help[*]}'${NC}"
+    echo "I don't know what you mean by: '$info'"
+    echo
+    echo -e "If you need help with this program, type: ${GREEN}'${help[*]}'${NC}"
   fi
 }
+
 # Call the functions to execute the program
 startOfProgram
-
