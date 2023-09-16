d61a6c395583a3a16b3cf9f3e71ec9fa626b20ab
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-15 20:31:39 -0400

    password list and ip address

--- etc/ipaddress.py
+++ etc/ipaddress.py
@@ -7,6 +7,7 @@ def get_mac_address():
         mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
         mac_address = ':'.join(mac[i:i+2] for i in range(0, 12, 2))
         return mac_address
+    
     except Exception as e:
         return str(e)
 
