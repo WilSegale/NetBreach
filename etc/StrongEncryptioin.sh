6afd26ddb542a64c74684c0193ddaf745d3f4d6b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-17 17:22:31 -0400

    strong encrypt

---
+++ etc/StrongEncryptioin.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Encrypt a file
+encrypt_file() {
+    input_file="$1"
+    output_file="$2"
+    password="$3"
+
+    openssl enc -aes-256-cbc -salt -in "$input_file" -out "$output_file" -pass pass:"$password"
+}
+
+# Decrypt a file
+decrypt_file() {
+    input_file="$1"
+    output_file="$2"
+    password="$3"
+
+    openssl enc -d -aes-256-cbc -in "$input_file" -out "$output_file" -pass pass:"$password"
+}
+
+# Usage example
+read -p "Input file name to encrypt: " input_file
+output_file="encrypted.enc"
+read -sp "Input Password: " password
+
+encrypt_file "$input_file" "$output_file" "$password"
+echo "File encrypted to $output_file"
