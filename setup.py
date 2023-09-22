94c59a336946d6ffe387547b61010c3aeb4c4d00
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-22 13:00:10 -0400

    setup

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
-from sys import *
-from colorama import *
-import logging
-import datetime
 import os
+import datetime
+import logging
+from colorama import Fore, Style  # Import specific classes/functions from colorama
 
 current_time = datetime.datetime.now().time()
-
-# Format the time with AM/PM
 formatted_time = current_time.strftime("%H:%M:%S %p")
-logging.basicConfig(filename='ERROR.LOG', level=logging.INFO)
+
+# Configure logging
+logging.basicConfig(filename='ERROR.log', level=logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+logging.getLogger().addHandler(console_handler)
 
 GREEN = Fore.GREEN
 RED = Fore.RED
+RESET = Style.RESET_ALL
+BRIGHT = Style.BRIGHT
 
-RESET = Fore.RESET
-BRIGHT = Fore.BRIGHT
-
-if platform == "linux" or platform == "linux2":
-    os.system("sudo rm -rf mac")
-    os.system("sudo rm -rf setup.py")
-    print(f"{GREEN}[+] Done{RESET}")
-
-elif platform == "darwin":
-    os.system("sudo rm -rf linux")
-    os.system("sudo rm -rf setup.py")
-    print(f"{GREEN}[+] Done{RESET}")
-
+if os.name == "posix":
+    # Check for Linux or macOS (Darwin)
+    if os.uname().sysname == "Linux":
+        os.system("sudo rm -rf mac")
+    elif os.uname().sysname == "Darwin":
+        os.system("sudo rm -rf linux")
+    else:
+        logging.error(f'{formatted_time} Your system is not supported.')
+        print(f'{BRIGHT}{RED}[-] Your system is not supported{RESET}')
 else:
     logging.error(f'{formatted_time} Your system is not supported.')
-    print(f'{BRIGHT}{RED}[-]Your system is not supported{RESET}')
+    print(f'{BRIGHT}{RED}[-] Your system is not supported{RESET}')
