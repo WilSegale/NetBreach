1e3e0999b6a32efce297691a5078142aaf9e070f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-30 17:43:55 -0400

    setup

--- setup.py
+++ setup.py
@@ -16,15 +16,18 @@ GREEN = Fore.GREEN
 RED = Fore.RED
 RESET = Style.RESET_ALL
 BRIGHT = Style.BRIGHT
+
 def CheckOS():
     # Check for Linux or macOS (Darwin)
     if os.uname().sysname == "Linux":
         os.system("sudo rm -rf mac")
+        os.system("sudo rm -rf setup.py")
         print("[+] DONE!")
     elif os.uname().sysname == "Darwin":
         os.system("sudo rm -rf linux")
+        os.system("sudo rm -rf setup.py")
         print("[+] DONE!")
     else:
         logging.error(f'{formatted_time} Your system is not supported.')
         print(f'{BRIGHT}{RED}[-] Your system is not supported{RESET}')
-CheckOS
+CheckOS()
