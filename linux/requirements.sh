8fbf6ad9a01140c35b8ed728ffe15c81c1fadd35
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-01 10:36:24 -0400

    req

--- linux/requirements.sh
+++ linux/requirements.sh
@@ -7,6 +7,7 @@ BRIGHT='\033[1m'
 NC='\033[0m' # No Color
 
 OS="linux"
+sudo apt-get update --force-yes
 if [ "$(id -u)" -eq 0 ]; then
     #notification message for the user to also if the local host has been compromised at all
     title="ERROR"
