953233ba1e11b1377ce5df41064577debc251518
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-09 14:24:45 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 import os
 import datetime
-import logging
+import subprocess
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BRIGHT='\033[1m'
-RESET='\033[0m'
+ERROR = open("ERROR.log", "a")
+
+# Terminal commands
+MacOS_Command = "sudo rm -rf linux && sudo rm -rf setup.py"
+LinuxCommand = "sudo rm -rf mac && sudo rm -rf setup.py"
+
+RED = '\033[0;31m'
+GREEN = '\033[0;32m'
+BRIGHT = '\033[1m'
+RESET = '\033[0m'
 
 current_time = datetime.datetime.now().time()
 formatted_time = current_time.strftime("%H:%M:%S %p")
 
-# Configure logging
-logging.basicConfig(filename='ERROR.log', level=logging.INFO)
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-logging.getLogger().addHandler(console_handler)
-
-
 def CheckOS():
     # Check for Linux or macOS (Darwin)
     if os.uname().sysname == "Linux":
-        os.system("sudo rm -rf mac")
-        os.system("sudo rm -rf setup.py")
+        subprocess.run(LinuxCommand, shell=True, check=True)
         print("[+] DONE!")
     elif os.uname().sysname == "Darwin":
-        os.system("sudo rm -rf linux")
-        os.system("sudo rm -rf setup.py")
+        subprocess.run(MacOS_Command, shell=True, check=True)
         print("[+] DONE!")
     else:
-        logging.error(f'{formatted_time} Your system is not supported.')
+        print(f"{formatted_time} Your system is not supported.", file=ERROR)
         print(f'{BRIGHT}{RED}[-] Your system is not supported{RESET}')
-CheckOS()
+
+# Close the ERROR.log file when done
+with open("ERROR.log", "a") as ERROR:
+    CheckOS()
