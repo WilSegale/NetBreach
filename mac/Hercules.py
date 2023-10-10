debbe091189ac1528dfa677df5f1ec8ef29f3fc5
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-10 19:44:01 -0400

    scritp

--- mac/Hercules.py
+++ mac/Hercules.py
@@ -55,6 +55,64 @@ try:
         print(info)
         print()
 
+    # connectes to the internet and checks and scans the prots on the internwork
+    elif len(sys.argv) == 2 and sys.argv[1] == "--global":
+        # gets the current time and formats it HH:MM:SS
+        current_time = datetime.datetime.now().time()
+
+        formatted_time = current_time.strftime("%I:%M:%S %p")
+
+        # Get the current date
+        current_date = datetime.datetime.now().strftime("%m/%d/%Y")
+
+        # easy way to read the root user function
+        ROOT = 0
+
+        def connect(url="https://google.com"):
+            try:
+                urllib.request.urlopen(url)  # Try to open a connection to the host
+                return True  # If successful, return True
+            except:
+                return False  # If unsuccessful, return False
+
+        # Makes sure that the user is connected to the internet    
+
+        if platform.system() == OS:
+            #checks if the user is running as root
+            if os.geteuid() == ROOT:
+                #makes the loading bar visible
+                def print_loading_bar(iterations, delay=0.1, width=40):
+                    """
+                    Prints a loading bar with green dots to visualize progress.
+                    
+                    Args:
+                        iterations (int): Total number of iterations.
+                        delay (float, optional): Delay between updates in seconds. Default is 0.1 seconds.
+                        width (int, optional): Width of the loading bar. Default is 40 characters.
+                    """
+                    for loadingBar in range(iterations + 1):
+                        progress = loadingBar / iterations  # Calculate the progress ratio
+                        bar_length = int(progress * width)  # Calculate the number of dots for the current progress
+                        bar = GREEN + '•' * bar_length + RESET + ' ' * (width - bar_length)  # Construct the loading bar string
+                        percentage = int(progress * 100)  # Calculate the percentage of completion
+                        
+                        # Print the loading bar and percentage, replacing the line each iteration
+                        print(f'\rLoading {ProgramName} [{bar}] {percentage} % ', end='', flush=False)
+                        
+                        time.sleep(delay)  # Pause to control the update rate
+                print_loading_bar(50)
+                os.system("bash localScript.sh")  # the script to run after loading
+            
+            else:    
+                # makes a pop up dialog to tell the user that the user is not root
+                print(f"TIME:{formatted_time} Please run as ROOT. DATE:{current_date}")
+                print(f"ERROR:TIME:{formatted_time} Please run as ROOT. DATE:{current_date}", file=ERROR)
+        else:
+            # makes a pop up dialog to tell the user that the OS is not correct
+            # makes a pop up dialog to tell the user that the OS is not correct
+            print(f"TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}")
+            print(f"WARNING:TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}",file=ERROR)
+    
     # puts the script in local mode
     elif len(sys.argv) == 2 and sys.argv[1] == "--local":
         # gets the current time and formats it HH:MM:SS
@@ -115,66 +173,7 @@ try:
     
     # puts the program in global mode
     else:
-        # gets the current time and formats it HH:MM:SS
-        current_time = datetime.datetime.now().time()
-
-        formatted_time = current_time.strftime("%I:%M:%S %p")
-
-        # Get the current date
-        current_date = datetime.datetime.now().strftime("%m/%d/%Y")
-
-        # easy way to read the root user function
-        ROOT = 0
-
-        def connect(host="google.com"):
-            try:
-                urllib.request.urlopen("http://" + host)  # Try to open a connection to the host
-                return True  # If successful, return True
-            except:
-                return False  # If unsuccessful, return False
-
-        # Makes sure that the user is connected to the internet    
-        if connect() == True:
-            #checks if the user is on Mac OS
-            if platform.system() == OS:
-                #checks if the user is running as root
-                if os.geteuid() == ROOT:
-                    #makes the loading bar visible
-                    def print_loading_bar(iterations, delay=0.1, width=40):
-                        """
-                        Prints a loading bar with green dots to visualize progress.
-                        
-                        Args:
-                            iterations (int): Total number of iterations.
-                            delay (float, optional): Delay between updates in seconds. Default is 0.1 seconds.
-                            width (int, optional): Width of the loading bar. Default is 40 characters.
-                        """
-                        for loadingBar in range(iterations + 1):
-                            progress = loadingBar / iterations  # Calculate the progress ratio
-                            bar_length = int(progress * width)  # Calculate the number of dots for the current progress
-                            bar = GREEN + '•' * bar_length + RESET + ' ' * (width - bar_length)  # Construct the loading bar string
-                            percentage = int(progress * 100)  # Calculate the percentage of completion
-                            
-                            # Print the loading bar and percentage, replacing the line each iteration
-                            print(f'\rLoading {ProgramName} [{bar}] {percentage} % ', end='', flush=False)
-                            
-                            time.sleep(delay)  # Pause to control the update rate
-                    print_loading_bar(50)
-                    os.system("bash start.sh")  # the script to run after loading
-                
-                else:    
-                    # makes a pop up dialog to tell the user that the user is not root
-                    print(f"TIME:{formatted_time} Please run as ROOT. DATE:{current_date}")
-                    print(f"ERROR:TIME:{formatted_time} Please run as ROOT. DATE:{current_date}", file=ERROR)
-            else:
-                # makes a pop up dialog to tell the user that the OS is not correct
-                print(f"TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}")
-                print(f"WARNING:TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}",file=ERROR)
-
-        else:
-            print(f"TIME:{formatted_time} Please connect to the internet. DATE:{current_date}")
-            print(f"ERROR:TIME:{formatted_time} Please connect to the internet. DATE:{current_date}",file=ERROR)
-
+        print("Please use the correct number of arguments. Example [--global] [--local] [--help] [-h]")
 # if the user uses control-c, the program will exit
 except KeyboardInterrupt:
     print("\n[-]Exiting...")
