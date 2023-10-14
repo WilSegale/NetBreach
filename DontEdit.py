3237ce94042bf850557667357e0ad53049b06456
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-14 09:32:52 -0400

    setup

--- DontEdit.py
+++ DontEdit.py
@@ -1,7 +1,3 @@
 from datetime import *
 import os
 import subprocess
-RED = '\033[0;31m'
-GREEN = '\033[0;32m'
-BRIGHT = '\033[1m'
-RESET = '\033[0m'
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from DontEdit import *
+import os
+import subprocess
+import datetime
+RED = '\033[0;31m'
+GREEN = '\033[0;32m'
+BRIGHT = '\033[1m'
+RESET = '\033[0m'
+
 ERROR = open("ERROR.log", "a")
 
-# Terminal commands
+# Define constants like BRIGHT, RED, and RESET if necessary
+
 MacOS_Command = "sudo rm -rf linux && sudo rm -rf setup.py"
 LinuxCommand = "sudo rm -rf mac && sudo rm -rf setup.py"
-
 
 current_time = datetime.datetime.now().time()
 formatted_time = current_time.strftime("%H:%M:%S %p")
@@ -19,8 +26,8 @@ def CheckOS():
         print("[+] DONE!")
     else:
         print(f"{formatted_time} Your system is not supported.", file=ERROR)
-        print(f'{BRIGHT}{RED}[-] Your system is not supported{RESET}')
+        print('[-] Your system is not supported')
 
-# Close the ERROR.log file when done
+# Close the ERROR.log file when done using the with statement
 with open("ERROR.log", "a") as ERROR:
     CheckOS()
