091f1a1f2e9d5affe247be3a6a1dc1c7602bdaf1
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-10 20:10:25 -0500

    dont edit and uninstall

--- mac/DontEdit.sh
+++ mac/DontEdit.sh
@@ -18,8 +18,9 @@ empty=("")
 alphabet=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
 
 # The yes array that contains the yes input
-yes=("yes" "YES" "y" "Y")
-no=("no" "NO" "n" "N")
+yes=("YES" "Y" "yes" "y")
+no=("NO" "N" "no" "n")
+
 # The array that contains the exit input
 exit=("exit" "quit" "EXIT" "QUIT" "STOP" "stop")
 
--- mac/uninstall.sh
+++ mac/uninstall.sh
@@ -1,124 +1,109 @@
-
 #!/bin/bash
 
-#imports the dont edit file to see what os the program is using
-source DontEdit.sh
+# Color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BRIGHT='\033[1m'
+NC='\033[0m' # No Color
 
-# Color variables for formatting output
-RED='\033[0;31m'       # Red color
-GREEN='\033[0;32m'     # Green color
-BRIGHT='\033[1m'       # Bright text
-NC='\033[0m'           # No color
+if [[ "$OSTYPE" == "darwin"* ]]; then
 
-# Check if the script is run as root (sudo)
-if [ "$(id -u)" -eq 0 ]; then
-    figlet "ERROR"  # Display an ASCII art error message
-    echo "Don't use sudo for this script." 
-    echo "Because it can damage your computer."
-    exit 1  # Exit with an error code
-else
-    # Check if the operating system is macOS
-    if [[ "$OSTYPE" == "${MAC}"* ]]; then
-        
-        # Prompt the user to confirm if they want to remove packages
-        echo -e "${RED}${BRIGHT}!Are you sure you want to remove your Packages (YES/NO)!:${NC}"
-        read -p ">>> " YES_NO
+    yes=("yes" "Yes" "YES")
 
-        if [[ "${yes[*]}" == *"$YES_NO"* ]]; then
-            # Check if the user is connected to the internet
-            if ping -q -c 1 -W 1 google.com >/dev/null; then 
-                # List of Brew packages that will be uninstalled if installed
-                Packages=(
-                    "wget"
-                    "hydra"
-                    "nmap"
-                    "mysql"
-                    "figlet"
-                    "zenity"
-                )
+    echo -e "${RED}${BRIGHT}!Are you sure you want to remove your Packages (YES/NO)!: ${NC}"
 
-                # List of PIP packages that will be uninstalled if installed
-                pipPackages=(
-                    "asyncio"
-                    "colorama"
-                )
+    read -p ">>> " YES_NO
 
-                # Function to check and uninstall a package
-                check_package() {
-                    package_name="$1"
-                    if command -v "$package_name" >/dev/null 2>&1; then
-                        echo "$package_name is installed."
-                        brew uninstall "$package_name"
+    if [[ "${yes[*]}" == *"$YES_NO"* ]]; then
+        # Check if Homebrew is installed
+        if command -v brew >/dev/null 2>&1; then
+            # Packages to check for installation
+            Packages=(
+                "wget"
+                "hydra"
+                "nmap"
+                "mysql"
+                "figlet"
+                "zenity"
+            )
+
+            # PIP packages that will be uninstalled if they are installed
+            pipPackages=(
+                "asyncio"
+                "colorama"
+                "pyfiglet"
+            )
+
+            # Function to check and uninstall a package
+            check_package() {
+                package_name="$1"
+                if brew list --formula | grep -q "$package_name"; then
+                    echo "$package_name is installed."
+                    brew uninstall "$package_name"
+                else
+                    echo -e "${RED}$package_name${NC} is not installed."
+                fi
+            }
+
+            # Check packages
+            for package in "${Packages[@]}"
+            do
+                check_package "$package"
+            done
+
+            # Uninstall PIP packages
+            for pipPackage in "${pipPackages[@]}"
+            do
+                if pip3 show "$pipPackage" >/dev/null 2>&1; then
+                    pip3 uninstall "$pipPackage" -y
+
+                    # Check the exit status of the last command
+                    if [ $? -ne 0 ]; then
+                        echo -e "Error occurred during uninstallation of \"$pipPackage\""
+                        exit 1
                     else
-                        echo -e "${RED}$package_name${NC} is not installed."
+                        echo -e "${pipPackage}: uninstalled ${GREEN}successfully${NC}"
                     fi
-                }
+                else
+                    echo -e "${RED}${pipPackage}${NC}: is not installed"
+                fi
+            done
 
-                # Check and uninstall Brew packages
+            # Check the exit status of the last command
+            if [ $? -ne 0 ]; then
+                echo -e "The packages that are removed are: ${GREEN}"
                 for package in "${Packages[@]}"
                 do
-                    check_package "$package"
+                    echo -e "$package"
                 done
-
-                # Uninstall PIP packages
+                echo -e "________PIP Packages________"
                 for pipPackage in "${pipPackages[@]}"
                 do
-                    if pip3 show "$pipPackage" >/dev/null 2>&1; then
-                        pip3 uninstall "$pipPackage" -y
-                        # Check the exit status of the last command
-                        if [ $? -ne 0 ]; then
-                            echo -e "Error occurred during uninstallation of \"$pipPackage\""
-                            exit 1
-                        else
-                            echo -e "${pipPackage}: uninstalled ${GREEN}successfully${NC}"
-                        fi
-                    else
-                        echo -e "${RED}${pipPackage}${NC}: is not installed"
-                    fi
+                    echo -e "$pipPackage"
                 done
-
-                # Uninstall PIP itself
-                pip3 uninstall -y pip
-
-                # Check the exit status of the last command
-                if [ $? -ne 0 ]; then
-                    echo -e "The packages that are removed are: ${GREEN}"
-                    for package in "${Packages[@]}"
-                    do
-                        echo -e "$package"
-                    done
-                    echo -e "________PIP Packages________"
-                    for pipPackage in "${pipPackages[@]}"
-                    do
-                        echo -e "${pipPackage}"
-                    done
-
-                    echo -e "________ERROR________"
-                    echo -e "${RED}Error occurred during pip uninstallation${NC}"
-                else
-                    echo -e "pip: uninstalled ${GREEN}successfully${NC}"
-                    echo -e "The packages that are removed are: ${GREEN}"
-                    for package in "${Packages[@]}"
-                    do
-                        echo -e "${package}"
-                    done
-                    echo -e "________PIP Packages________"
-                    for pipPackage in "${pipPackages[@]}"
-                    do
-                        echo -e "${pipPackage}"
-                    done
-                fi
+                echo -e "________ERROR________"
+                echo -e "${RED}Error occurred during pip uninstallation${NC}"
             else
-                echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
+                echo -e "The packages that are removed are: ${GREEN}"
+                for package in "${Packages[@]}"
+                do
+                    echo -e "${package}"
+                done
+                echo -e "________PIP Packages________"
+                for pipPackage in "${pipPackages[@]}"
+                do
+                    echo -e "${pipPackage} is removed"
+                done
             fi
 
-        elif [[ "${no[*]}" == *"$YES_NO"* ]]; then
-            echo -e "[-] Ok, I will not remove the packages."
         else
-            echo -e "${RED}Please enter a valid option${NC}"
-            exit 1
+            echo -e "${RED}ERROR: Homebrew is not installed.${NC}"
         fi
+
     else
-        echo -e "${RED}${BRIGHT}This script can only be run on Mac OS${NC}"
+        echo -e "Ok, I will not remove the packages."
     fi
+
+else
+    echo -e "${RED}${BRIGHT}This script can only be run on macOS${NC}"
 fi
