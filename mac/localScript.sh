6592a60f19c9f2d3dc2bbabb2e18b022a976194c
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-17 12:21:17 -0500

    local script

--- mac/localScript.sh
+++ mac/localScript.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
+
+#holds the file that contains importent funcitons for the porgram to work
+source DontEdit.sh
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 # OS of the computer
 OS="darwin"
-
-# Root user
-root=0
-
-# the array that holds nothing in it
-empty=("")
-
-alphabet=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
-
-# The yes array that contains the yes input
-yes=("yes" "YES" "y" "Y")
-
-# The array that contains the exit input
-exit=("exit" "quit" "EXIT" "QUIT" "STOP" "stop")
 
 # Gets the current time in a 12-hour format
 CURRENT_TIME=$(date +"%I:%M:%S %p")
