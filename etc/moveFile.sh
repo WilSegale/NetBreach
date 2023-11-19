e43b9d47c19b87ba818421e8b214f55699e87fc0
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-19 14:22:02 -0500

    move file

--- etc/moveFile.sh
+++ etc/moveFile.sh
@@ -1,5 +1,7 @@
-read -p "Input the usersname for the file transfer: " username
+read -p "Input the username for the file transfer: " username
 read -p "Input the IP address for the file transfer: " ipaddress
-read -p "Input the file you want to transfer: " file
-read -p "Input the destination for the file to goto: " destination
-scp $username@$ipaddress:$file $destination
+read -p "Input the folder you want to transfer: " folder
+read -p "Input the destination for the file to go to: " destination
+
+# Use quotes around variables to handle paths with spaces
+scp -r "${folder}" "${username}"@"${ipaddress}":"${destination}"
