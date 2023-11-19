57da48456a2e6af071cc8f48b95221c49f51f60d
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-19 13:31:36 -0500

    ds

---
+++ linux/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+start(){
+  selected_items=$(zenity --list --text "Select Version" --column "Version" \
+    "Gui with temrinal(1)" \
+    "Terminal only(2)")
+  # Check if the selected item contains "22"
+  
+  if [[ "${selected_items}" == *"1"* ]]; then
+    sudo bash GuiScript.sh
+
+  elif [[ "${selected_items}" == *"2"* ]]; then
+    sudo bash script.sh
+  fi
+  
+  }
+start
