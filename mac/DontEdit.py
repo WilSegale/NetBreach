ed365cc49f14c4945c7c8ee0f1d59a1627ba04cb
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-19 13:43:05 -0500

    dont edit

--- mac/DontEdit.py
+++ mac/DontEdit.py
@@ -17,7 +17,7 @@ RED = "\033[91m"
 RESET = "\033[0m"
 
 # Define constants for command-line arguments
-HELP = ["--help", "-h"]
-GUI = ["--GUI", "--gui"]
-GLOBAL = ["--global", "--GLOBAL"]
-LOCAL = ["--local", "--LOCAL"]
+HELP = ["--HELP", "--Help", "--help", "-h", "-H"]
+GUI = ["--GUI", "--Gui", "--gui"]
+GLOBAL = ["--GLOBAL", "--Global", "--global"]
+LOCAL = ["--LOCAL", "--Local", "--local"]
