73605bd6fddf4ad0691d3baa8223a8e8369372f3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-12-03 10:08:08 -0500

    script

---
+++ mac/DontEdit.py
@@ -0,0 +1,24 @@
+# Import necessary modules
+import datetime
+import platform
+import time
+import os
+import urllib.request
+import logging
+import sys
+import subprocess
+
+# for detecting the OS module
+OS='Darwin'
+
+# Define color codes for console output
+BRIGHT = '\033[1m'
+GREEN = "\033[92m"
+RED = "\033[91m"
+RESET = "\033[0m"
+
+# Define constants for command-line arguments
+HELP = ["--HELP", "--Help", "--help", "-h", "-H"]
+GUI = ["--GUI", "--Gui", "--gui"]
+GLOBAL = ["--GLOBAL", "--Global", "--global"]
+LOCAL = ["--LOCAL", "--Local", "--local"]
