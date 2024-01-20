092494053ce5cb1d0f2eb1a4676902dd539c0124
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-01-20 09:02:52 -0500

    global script update

--- mac/GlobalScript.sh
+++ mac/GlobalScript.sh
@@ -214,7 +214,7 @@ else
                         Connected_To_SSH_SERVER="We have connected you to ${user}. Please enter the password to ${user} to continue..."
                         echo "${title}"
                         echo "${Connected_To_SSH_SERVER}"
-                        ssh $user@$host -p $port
+                        ssh "${user}@${host}" -p "${port}"
                     fi
                 fi
             }
