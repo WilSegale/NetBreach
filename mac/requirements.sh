2d2926d3e008a32676da58cdd2cd0d3471f813ad
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-02-03 10:52:01 -0500

    requirements.sh

--- mac/requirements.sh
+++ mac/requirements.sh
@@ -29,64 +29,58 @@ requiredments() {
         echo "+++++++++++++++++++++++++++++++++++++++++"
         exit 1
     else
-        # Helps the users understand what the program does
-        if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-            echo "This script will install the packages for it to work properly"
-            echo
-        else
-            # Check if the OS is macOS
-            if [[ "${OSTYPE}" == "darwin"* ]]; then
-                if ping -c 1 google.com >/dev/null 2>&1; then
-                    # Install package using Homebrew package manager
-                    install_brew_package() {
-                        package_name="$1"
-                        if ! command -v "${package_name}" >/dev/null 2>&1; then
-                            brew install "${package_name}"
-                        else
-                            echo -e "${package_name} is already ${GREEN}installed.${NC}"
-                        fi
-                    }
+        # Check if the OS is macOS
+        if [[ "${OSTYPE}" == "darwin"* ]]; then
+            if ping -c 1 google.com >/dev/null 2>&1; then
+                # Install package using Homebrew package manager
+                install_brew_package() {
+                    package_name="$1"
+                    if ! command -v "${package_name}" >/dev/null 2>&1; then
+                        brew install "${package_name}"
+                    else
+                        echo -e "${package_name} is already ${GREEN}installed.${NC}"
+                    fi
+                }
 
-                    # Install package using pip
-                    install_pip_package() {
-                        package_name="$1"
-                        if ! python3 -m pip show "${package_name}" >/dev/null 2>&1; then
-                            pip3 install "${package_name}"
-                        else
-                            echo -e "${package_name} is already ${GREEN}installed.${NC}"
-                        fi
-                    }
+                # Install package using pip
+                install_pip_package() {
+                    package_name="$1"
+                    if ! python3 -m pip show "${package_name}" >/dev/null 2>&1; then
+                        pip3 install "${package_name}"
+                    else
+                        echo -e "${package_name} is already ${GREEN}installed.${NC}"
+                    fi
+                }
 
-                    echo
-                    echo "_________BREW PACKAGES________"
+                echo
+                echo "_________BREW PACKAGES________"
 
-                    # Install Homebrew packages
-                    for package in "${Packages[@]}"; do
-                        install_brew_package "${package}"
-                    done
+                # Install Homebrew packages
+                for package in "${Packages[@]}"; do
+                    install_brew_package "${package}"
+                done
 
-                    echo
-                    echo "_________PIP PACKAGES________"
-                    # Install PIP packages
-                    for PIP in "${pipPackages[@]}"; do
-                        install_pip_package "${PIP}"
-                    done
+                echo
+                echo "_________PIP PACKAGES________"
+                # Install PIP packages
+                for PIP in "${pipPackages[@]}"; do
+                    install_pip_package "${PIP}"
+                done
 
-                    echo
+                echo
 
-                    # Update PIP
-                    echo "_________PIP UPDATES________"
-                    python3 -m pip install --upgrade pip
+                # Update PIP
+                echo "_________PIP UPDATES________"
+                python3 -m pip install --upgrade pip
 
-                    echo
-                    successful_MESSAGE="${GREEN}[+]${NC} All packages are installed successfully"
-                    echo -e "${successful_MESSAGE}"
-                else
-                    echo -e "${RED}ERROR:${NC} NOT CONNECTED TO THE INTERNET"
-                fi
+                echo
+                successful_MESSAGE="${GREEN}[+]${NC} All packages are installed successfully"
+                echo -e "${successful_MESSAGE}"
             else
-                echo -e "${RED}${BRIGHT}[-]${NC} Wrong OS, please use the correct OS." # If the user is not using the right OS, it says "You are using the wrong OS"
+                echo -e "${RED}ERROR:${NC} NOT CONNECTED TO THE INTERNET"
             fi
+        else
+            echo -e "${RED}${BRIGHT}[-]${NC} Wrong OS, please use the correct OS." # If the user is not using the right OS, it says "You are using the wrong OS"
         fi
     fi
 }
