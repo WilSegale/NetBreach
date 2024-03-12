c4226c88ecda8a6afabef11ad85014fce1a85319
zero <zero@zeros-Air.fios-router.home>
2024-03-12 15:52:43 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ import subprocess
 import datetime
 import logging
 import os
+import time
+
+GREEN = "\033[92m"
+
+
+RESET = "\033[0m"
 
 # Set up logging 
 logger = logging.getLogger(__name__)
@@ -24,12 +30,32 @@ if __name__ == "__main__":
             platform = "linux"
         elif os.uname().sysname == "Darwin":
             platform = "macos"
-    else:
-        logger.error("Unsupported platform")
-        exit(1)
+        else:
+            logger.error("Unsupported platform")
+            exit(1)
 
     # Run command
     if platform == "linux":
+        def print_loading_bar(iterations, delay=0.1, width=40):
+            """
+            Prints a loading bar with green dots to visualize progress.
+            
+            Args:
+                iterations (int): Total number of iterations.
+                delay (float, optional): Delay between updates in seconds. Default is 0.1 seconds.
+                width (int, optional): Width of the loading bar. Default is 40 characters.
+            """
+            for loadingBar in range(iterations + 1):
+                progress = loadingBar / iterations  # Calculate the progress ratio
+                bar_length = int(progress * width)  # Calculate the number of dots for the current progress
+                bar = GREEN + '•' * bar_length + RESET + ' ' * (width - bar_length)  # Construct the loading bar string
+                percentage = int(progress * 100)  # Calculate the percentage of completion
+                
+                # Print the loading bar and percentage, replacing the line each iteration
+                print(f'\rRunning Setup.py [{bar}] {percentage} % ', end='', flush=False)
+                
+                time.sleep(delay)  # Pause to control the update rate
+        print_loading_bar(50)
         print("DONE running linux command")
         logger.info(f"Running command: {' '.join(linux_command)}")
         try:
