80a9e1be3225b481e9cc1eb23966b9f1cb351b42
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-04-26 16:10:15 -0400

    update

--- MacOs/GlobalScript.sh
+++ MacOs/GlobalScript.sh
@@ -115,9 +115,9 @@ else
             clear
 
             # Tells the user if they want to crack the ports that are listed in the prompt or have help if they are stuck on what to do
-            Hercules() {
+            NetBreach() {
                 # The logo of the program
-                figlet -f slant "Hercules"
+                figlet -f slant "NetBreach"
                 echo "Type the number of the port you want to scan (SSH - 22, VNC - 5900, MySQL - 3306). To scan all, type 'ALL'"
                 echo "If you want to stop the program type 'stop'."
                 read -p ">>> " service
@@ -201,7 +201,7 @@ else
                     if [[ $user == "" && $host == "" || $host == "" ]]; then
                         # No service specified, re-prompt for input
                         echo "No service specified"
-                        Hercules
+                        NetBreach
                     # If the user inputs something in the 'Input Username' function and the hostname function,
                     # it will continue as normal
                     else
@@ -237,7 +237,7 @@ else
                     if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
                         # No service specified, re-prompt for input
                         echo "No service specified"
-                        Hercules
+                        NetBreach
                     # If the user inputs something in the 'Input Username' function and the hostname function,
                     # it will continue as normal
                     else
@@ -271,7 +271,7 @@ else
                     if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
                         # No service specified, re-prompt for input
                         echo "No service specified"
-                        Hercules
+                        NetBreach
                     # If the user inputs something in the 'Input Username' function and the hostname function,
                     # it will continue as normal
                     else
@@ -284,7 +284,7 @@ else
                 fi
             }
 
-            Hercules # Calls the Hercules function
+            NetBreach # Calls the NetBreach function
 
             RunHackingCommand # Calls the RunHackingCommand function
 
