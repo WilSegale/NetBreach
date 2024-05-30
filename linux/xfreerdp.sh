6d60924b1d2f9e11c8a87c07ded9cc0157653428
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-05-29 20:48:15 -0400

    Update xfreerdp.sh

--- linux/xfreerdp.sh
+++ linux/xfreerdp.sh
@@ -8,23 +8,24 @@ ConnectXfreerdp(){
 
     # Check if file exists
     if [ -e "${FILE}" ]; then
-        source $FILE
+        source connections.conf
         xfreerdp $@
     else
         sudo nmap -sS 192.168.1.1/24 -Pn -oN scan.txt --open
         echo
-        read -p "Input username: " username
         read -p "Input IP: " ip
+        read -p "Input username: " username
         read -s -p "Input password: " password
+        echo
         read -p "Do you want to save this connection? (y/n) " save
         if [ $save == "y" ]; then
-            echo "/u:${username}" >> connections.conf
-            echo "/v:${ip}" >> connections.conf
-            echo "/p:${password}" >> connections.conf
+            echo "XFREERDP_IP${ip}" >> connections.conf
+            echo "XFREERDP_USERNAME=${username}" >> connections.conf
+            echo "XFREERDP_PASSWORD=${password}" >> connections.conf
             
             echo "Loading xfreerdp server..."
             sleep 1
-            xfreerdp /u:"${username}" /v:"${ip}" /p:"${password}"
+            xfreerdp /v:"${ip}" /u:"${username}" /p:"${password}"
 
         else
             # Put the
