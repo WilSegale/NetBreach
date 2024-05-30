89048f66f26aab06f81056da87c2abf58c39be67
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-05-29 20:34:10 -0400

    udpate

--- MacOs/xfreerdp.sh
+++ MacOs/xfreerdp.sh
@@ -1,17 +1,38 @@
-source DontEdit
+source DontEdit.sh
+clear
 #get to the xfreerdp connection
 ConnectXfreerdp(){
+
+    FILE="connections.txt"
     figlet -f slant "xfreerdp"
-    sudo nmap -sS 192.168.1.1/24 -Pn -oN scan.txt --open
-    echo
-    read -p "Input username: " username
-    read -p "Input IP: " ip
-    read -s -p "Input password: " password
-    # Put the
-    echo
-    echo "Loading xfreerdp server..."
-    sleep 1
-    xfreerdp /u:"${username}" /v:"${ip}" /p:"${password}"
-    exit
+
+    # Check if file exists
+    if [ -e "${FILE}" ]; then
+        xfreerdp @
+    else
+        sudo nmap -sS 192.168.1.1/24 -Pn -oN scan.txt --open
+        echo
+        read -p "Input username: " username
+        read -p "Input IP: " ip
+        read -s -p "Input password: " password
+        read -p "Do you want to save this connection? (y/n) " save
+        if [ $save == "y" ]; then
+            echo "/u:${username}" >> connections.conf
+            echo "/v:${ip}" >> connections.conf
+            echo "/p:${password}" >> connections.conf
+            
+            echo "Loading xfreerdp server..."
+            sleep 1
+            xfreerdp /u:"${username}" /v:"${ip}" /p:"${password}"
+
+        else
+            # Put the
+            echo
+            echo "Loading xfreerdp server..."
+            sleep 1
+            xfreerdp /u:"${username}" /v:"${ip}" /p:"${password}"
+            exit
+        fi
+    fi
 }
 ConnectXfreerdp
