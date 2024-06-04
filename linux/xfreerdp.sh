509a4281cb6504908bfc3708fcab5afa2c575590
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-06-04 14:05:50 -0400

    upaddte

--- linux/xfreerdp.sh
+++ linux/xfreerdp.sh
@@ -3,38 +3,44 @@ clear
 
 # Function to handle xfreerdp connection
 ConnectXfreerdp() {
-    sudo chmod +x *
+    if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
 
-    FILE="connections.env"
-    figlet -f slant "xfreerdp"
 
-    # Check if file exists
-    if [ -e "${FILE}" ]; then
-        source "${FILE}"
-        xfreerdp /u:"${XFREERDP_USERNAME}" /v:"${XFREERDP_IP}" /p:"${XFREERDP_PASSWORD}"
-    else
-        sudo nmap -sS 192.168.1.1/24 -Pn -oN scan.txt --open
-        echo
-        read -p "Input IP: " ip
-        read -p "Input username: " username
-        read -s -p "Input password: " password
-        echo
-        read -p "Do you want to save this connection? (y/n) " save
-        if [[ "${save}" == "y" ]]; then
-            echo "XFREERDP_IP=${ip}" >> "${FILE}"
-            echo "XFREERDP_USERNAME=${username}" >> "${FILE}"
-            echo "XFREERDP_PASSWORD=${password}" >> "${FILE}"
-            
-            echo "Loading xfreerdp server..."
-            sleep 1
-            xfreerdp /v:"${ip}" /u:"${username}" /p:"${password}"
+        FILE="connections.env"
+        figlet -f slant "xfreerdp"
+
+        # Check if file exists
+        if [ -e "${FILE}" ]; then
+            source "${FILE}"
+            xfreerdp /u:"${XFREERDP_USERNAME}" /v:"${XFREERDP_IP}" /p:"${XFREERDP_PASSWORD}"
         else
+            sudo nmap -sS 192.168.1.1/24 -Pn -oN scan.txt --open
             echo
-            echo "Loading xfreerdp server..."
-            sleep 1
-            xfreerdp /u:"${username}" /v:"${ip}" /p:"${password}"
-            exit
+            read -p "Input IP: " ip
+            read -p "Input username: " username
+            read -s -p "Input password: " password
+            echo
+            read -p "Do you want to save this connection? (y/n) " save
+            if [[ "${save}" == "y" ]]; then
+                echo "XFREERDP_IP=${ip}" >> "${FILE}"
+                echo "XFREERDP_USERNAME=${username}" >> "${FILE}"
+                echo "XFREERDP_PASSWORD=${password}" >> "${FILE}"
+                
+                echo "Loading xfreerdp server..."
+                sleep 1
+                xfreerdp /v:"${ip}" /u:"${username}" /p:"${password}"
+            else
+                echo
+                echo "Loading xfreerdp server..."
+                sleep 1
+                xfreerdp /u:"${username}" /v:"${ip}" /p:"${password}"
+                exit
+            fi
         fi
+    else
+        echo -e "[ ${RED}FAIL${NC} ]This script can only be run on Linux"
+        exit
+
     fi
 }
 
