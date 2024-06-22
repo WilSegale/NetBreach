3bbfa145e4d3c6bc42e249c8b097eb91554340d2
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-06-22 11:27:24 -0400

    Update InstalledPackages.sh

--- linux/InstalledPackages.sh
+++ linux/InstalledPackages.sh
@@ -3,10 +3,10 @@ source DontEdit.sh
 # Get the list of installed packages and store them in an array
 # Loop through each package and check if it is installed
 for Packages in "${Packages[@]}"
-
-    if dpkg -l | grep -q "^ii  $Packages "; then
-        echo "$Packages is installed"
-    else
-        echo "$Packages is NOT installed"
-    fi
+    do
+        if dpkg -l | grep -q "^ii  $Packages "; then
+            echo "$Packages is installed"
+        else
+            echo "$Packages is NOT installed"
+        fi
 done
