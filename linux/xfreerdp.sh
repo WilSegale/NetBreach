fcd8c02f8952a688082865b081192390d0a95c76
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-06-23 15:48:44 -0400

    Update xfreerdp.sh

--- linux/xfreerdp.sh
+++ linux/xfreerdp.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 source DontEdit.sh
 clear
+# quits program with ctrl-c
+EXIT_PROGRAM_WITH_CTRL_C() {
+    echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
+    # Add cleanup commands here
+    exit 1
+}
 
+# quits program with ctrl-z
+EXIT_PROGRAM_WITH_CTRL_Z(){
+    echo ""
+    echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
+    # Add cleanup commands here
+    exit 1
+}
+
+# Function to be executed when Ctrl+Z is pressed
+handle_ctrl_z() {
+    EXIT_PROGRAM_WITH_CTRL_Z
+    exit 1
+    # Your custom action goes here
+}
+
+# Set up the trap to call the function on SIGTSTP (Ctrl+Z)
+trap 'handle_ctrl_z' SIGTSTP
+
+# Function to handle Ctrl+C
+ctrl_c() {
+    echo ""
+    EXIT_PROGRAM_WITH_CTRL_C
+}
+
+trap ctrl_c SIGINT
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
 # Function to handle xfreerdp connection
 ConnectXfreerdp() {
     if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
