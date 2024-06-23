4562514d4a1a258cdf5595d939742bbc635de22c
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-06-23 15:30:42 -0400

    Update requirements.sh

--- MacOs/requirements.sh
+++ MacOs/requirements.sh
@@ -68,6 +68,7 @@ ctrl_c() {
 
 trap ctrl_c SIGINT
 
+# installes home brew for the brew pakcages to run
 InstallHomeBrew(){
     if command -v brew &>/dev/null; then
         echo
@@ -109,7 +110,25 @@ install_brew_package() {
 # Function to install package using pip
 install_pip_package() {
     package_name="$1"
+    
+    # Attempt to install the package
     python3 -m pip install --user --upgrade "${package_name}" --break-system-packages
+    
+    # Check the exit code of the installation
+    if [ $? -eq 0 ]; then
+        # Verify the package installation by trying to import it in Python
+        python3 -c "import ${package_name}" 2>/dev/null
+        
+        if [ $? -eq 0 ]; then
+            echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] ${package_name} installed and verified successfully."
+        else
+            echo -e "[ ${BRIGHT}${RED}ERROR${NC} ] ${package_name} installed but could not be imported in Python."
+            exit 1
+        fi
+    else
+        echo -e "[ ${RED}ERROR${NC} ] Failed to install ${package_name}."
+        exit 1
+    fi
 }
 
 # Function to upgrade pip
