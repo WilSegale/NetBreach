c0e52c749742e466b3328ba46e65fae5018d93d4
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-07-05 14:24:58 -0400

    Update requirements.sh

--- linux/requirements.sh
+++ linux/requirements.sh
@@ -60,7 +60,7 @@ checkForPackages() {
     fi
 }
 
-requiredments(){
+requirements(){
 # Check if the OS is Linux
 if [[ "${OSTYPE}" == "${Linux}"* ]]; then
     if ping -c 1 google.com >/dev/null 2>&1; then
@@ -94,7 +94,30 @@ if [[ "${OSTYPE}" == "${Linux}"* ]]; then
                 exit 1
             fi
         }
-
+        pipFail(){
+            if [ "$1" = "--pipForce" ]; then
+                package_name="$1"
+                
+                # Attempt to install the package
+                python3 -m pip3 install --user --upgrade "${package_name}" --break-system-packages
+                
+                # Check the exit code of the installation
+                if [ $? -eq 0 ]; then
+                    # Verify the package installation by trying to import it in Python
+                    python3 -c "import ${package_name}" 2>/dev/null
+                    
+                    if [ $? -eq 0 ]; then
+                        echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] ${package_name} installed and verified successfully."
+                    else
+                        echo -e "[ ${BRIGHT}${RED}FAIL${NC} ] ${package_name} installed but could not be imported in Python."
+                        exit 1
+                    fi
+                else
+                    echo -e "[ ${BRIGHT}${RED}FAIL${NC} ] Failed to install ${package_name}."
+                    exit 1
+                fi
+            fi
+        }
         # Function to upgrade pip
         upgrade_pip() {
             python3 -m pip install --upgrade pip
@@ -129,4 +152,4 @@ else
     echo -e "[ ${BRIGHT}${RED}FAIL${NC} ] Wrong OS, please use the correct OS."
 fi
 }
-requiredments
+requirements
