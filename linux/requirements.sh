2f644d694081fd329f603f6a4738889052460437
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-07-05 14:22:04 -0400

    Update requirements.sh

--- linux/requirements.sh
+++ linux/requirements.sh
@@ -76,7 +76,7 @@ if [[ "${OSTYPE}" == "${Linux}"* ]]; then
             package_name="$1"
             
             # Attempt to install the package
-            python3 -m pip install --user --upgrade "${package_name}" --break-system-packages
+            python3 -m pip3 install --user --upgrade "${package_name}" --break-system-packages
             
             # Check the exit code of the installation
             if [ $? -eq 0 ]; then
