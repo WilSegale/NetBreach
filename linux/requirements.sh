116eb9fb4814eaddf77f7d2034167b5768c9f65d
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-07-06 11:31:18 -0400

    Revert "update"
    This reverts commit a74426edf288c25243b917aa0c94d8183412c27d.

--- linux/requirements.sh
+++ linux/requirements.sh
@@ -67,12 +67,14 @@ if [[ "${OSTYPE}" == "${Linux}"* ]]; then
         # Function to install package using apt package manager
         install_linux_package() {
             package_name="$1"
-            for package_name in "${Packages[@]}"
+
+            for package in "${Packages[@]}"
             do
-                if dpkg -l | grep -q "^ii  ${package_name}\s"; then
-                    echo -e "${package_name} is ${GREEN}installed.${NC}"
+                if dpkg -l | grep -q "^ii  ${package} "; then
+                    sudo apt-get install "${package_name}" -y
+                    echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] ${package_name} installed successfully."
                 else
-                    echo -e "${package_name} is ${RED}not installed.${NC}"
+                    echo -e "[ ${BRGIHT}${RED}FAIL${NC} ] ${package}"
                 fi
             done
         }
@@ -82,7 +84,7 @@ if [[ "${OSTYPE}" == "${Linux}"* ]]; then
             package_name="$1"
             
             # Attempt to install the package
-            python3 -m pip install --user --upgrade "${package_name}" 
+            python3 -m pip install --user --upgrade "${package_name}" --break-system-packages
             
             # Check the exit code of the installation
             if [ $? -eq 0 ]; then
@@ -108,7 +110,7 @@ if [[ "${OSTYPE}" == "${Linux}"* ]]; then
                 package_name="$1"
                 
                 # Attempt to install the package
-                python3 -m pip install --user --upgrade "${package_name}" --break-system-packages
+                python3 -m pip install --user --upgrade "${package_name}"
                 
                 # Check the exit code of the installation
                 if [ $? -eq 0 ]; then
