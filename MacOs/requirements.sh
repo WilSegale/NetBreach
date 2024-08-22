3e073224d8fec9a3ef71614c6357ec91f1acbfa8
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-08-22 17:38:01 -0400

    update

--- MacOs/requirements.sh
+++ MacOs/requirements.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
+# Load DontEdit.sh if it exists
 if [ -f "DontEdit.sh" ]; then
     source DontEdit.sh
 else
     echo "DontEdit.sh not found!"
     exit 1
 fi
-
 
 # Wifi connection check function
 WifiConnection() {
@@ -38,248 +39,149 @@ EthernetConnection() {
                 InstallHomeBrew
             fi
         else
-            echo -e "[ ${RED}FAIL${NC} ] Ethernet (en0) or WiFi is not connected."
+            echo -e "[ ${RED}FAIL${NC} ] Ethernet (en0) is not connected."
         fi
     else
-        echo -e "[ ${RED}FAIL${NC} ] Ethernet (en0) or WiFi interface not found."
+        echo -e "[ ${RED}FAIL${NC} ] Ethernet (en0) interface not found."
     fi
 }
-# help function
+
+# Help function
 HELP(){
     echo "REQUIREMENTS HELP"
-
     echo "_________________"
     echo "This script is used to check if the system has the required packages installed."
     echo "If the pip packages fail to install type"
-    echo '''bash requrements.sh --pipForce'''
+    echo '''bash requirements.sh --pipForce'''
     echo "_________________"
-
     exit 1
 }
+
 # Install Homebrew if not already installed
 InstallHomeBrew() {
-    
+    echo "Checking Homebrew installation..."
     if command -v brew &>/dev/null; then
-        echo
+        echo -e "[ ${GREEN}OK${NC} ] Homebrew is already installed."
     else
         echo "Homebrew is not installed."
-        echo
         echo "Would you like to install Homebrew? (YES/NO)"
         read -p ">>> " answer
-        if [[ "${yes[*]}" == *"${answer}"* ]]; then
+        if [[ "${answer,,}" == "yes" ]]; then
             echo "Installing Homebrew..."
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
             sleep 1
             eval "$(/opt/homebrew/bin/brew shellenv)"
-            # Check the exit code of the previous command
             if [ $? -ne 0 ]; then
                 echo -e "[ ${RED}FAIL${NC} ] Homebrew installation failed."
             else
                 echo -e "[ ${GREEN}OK${NC} ] Homebrew installation successful."
                 echo "Run the program again to install the rest of the packages."
             fi
-        elif [[ "${no[*]}" == *"${answer}"* ]]; then
-            echo "Exiting the script."
         else
             echo "Exiting the script."
+            exit 1
         fi
-        exit 1
     fi
 }
 
-# Function to install package using brew package manager
+# Function to install a package using brew
 install_brew_package() {
     package_name="$1"
     if ! brew list --formula | grep -q "^${package_name}\$"; then
         brew install "${package_name}"
         if [ $? -eq 0 ]; then
-            echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] ${package_name} installed and verified successfully."
+            echo -e "[ ${GREEN}OK${NC} ] ${package_name} installed successfully."
         else
-            echo -e "[ ${BRIGHT}${RED}ERROR${NC} ] ${package_name} installed but could not be imported in Python."
+            echo -e "[ ${RED}ERROR${NC} ] ${package_name} installation failed."
         fi
     else
-        echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] ${package_name} is already installed."
+        echo -e "[ ${GREEN}OK${NC} ] ${package_name} is already installed."
     fi
 }
-pipFail(){
-    echo "pip force"
-    if [ "$1" = "--pipForce" ]; then
-        package_name="$1"
-        
-        # Attempt to install the package
-        python3 -m pip install --user --upgrade "${package_name}" --break-system-packages
-        
-        # Check the exit code of the installation
-        if [ $? -eq 0 ]; then
-            # Verify the package installation by trying to import it in Python
-            python3 -c "import ${package_name}" 2>/dev/null
-            
-            if [ $? -eq 0 ]; then
-                echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] ${package_name} installed and verified successfully."
-            else
-                echo -e "[ ${BRIGHT}${RED}FAIL${NC} ] ${package_name} installed but could not be imported in Python."
-                exit 1
-            fi
-        else
-            echo -e "[ ${BRIGHT}${RED}FAIL${NC} ] Failed to install ${package_name}."
-            exit 1
-        fi
-    fi
-}
-
 
 # Function to upgrade pip
 upgrade_pip() {
     python3 -m pip install --upgrade pip
-    echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] pip packages updated successfully."
+    echo -e "[ ${GREEN}OK${NC} ] pip upgraded successfully."
 }
-# Check if packages are installed
+
+# Function to check for installed packages
 checkForPackages() {
-    if [ $? -ne 0 ]; then
-        for package in "${Packages[@]}" 
-        do
-            echo -e "The packages that are installed are: ${package}"
-        done
-        echo ""
-        echo -e "________PIP Packages________"
-        for pipPackage in "${pipPackages[@]}" 
-        do
-            echo -e "${RED}${pipPackage}${NC}"
-        done
-        echo -e "________ERROR________"
-        echo -e "${RED}Error occurred during pip uninstallation${NC}"
-    else
-        for package in "${Packages[@]}"
-        do
-            if brew list --formula | grep -q "^${package}\$"; then
-                echo -e "${package}: ${GREEN}Is installed.${NC}"
-            else
-                echo -e "${package}: ${RED}not installed.${NC}"
-            fi
-        done
-        echo -e "________PIP Packages________"
-        for pipPackage in "${pipPackages[@]}" 
-        do
+    for package in "${Packages[@]}"; do
+        if brew list --formula | grep -q "^${package}\$"; then
+            echo -e "${package}: ${GREEN}Is installed.${NC}"
+        else
+            echo -e "${package}: ${RED}Not installed.${NC}"
+        fi
+    done
+
+    echo -e "________PIP Packages________"
+    for pipPackage in "${pipPackages[@]}"; do
+        if python3 -c "import ${pipPackage}" &>/dev/null; then
             echo -e "${pipPackage}: ${GREEN}Is Installed${NC}"
-        done
-    fi
+        else
+            echo -e "${pipPackage}: ${RED}Not Installed${NC}"
+        fi
+    done
 }
 
-
-# quits program with ctrl-c
-EXIT_PROGRAM_WITH_CTRL_C() {
-    echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
-    # Add cleanup commands here
-    exit 1
-}
-
-# quits program with ctrl-z
-EXIT_PROGRAM_WITH_CTRL_Z() {
-    echo ""
-    echo -e "${RED}[-]${NC} EXITING SOFTWARE..."
-    # Add cleanup commands here
-    exit 1
-}
-
-# Function to be executed when Ctrl+Z is pressed
-handle_ctrl_z() {
-    EXIT_PROGRAM_WITH_CTRL_Z
-    exit 1
-    # Your custom action goes here
-}
-
-# Set up the trap to call the function on SIGTSTP (Ctrl+Z)
-trap 'handle_ctrl_z' SIGTSTP
-
-# Function to handle Ctrl+C
-ctrl_c() {
-    echo ""
-    EXIT_PROGRAM_WITH_CTRL_C
-}
-
-trap ctrl_c SIGINT
-
-if [[ "$1" = "--help" || "$1" = "-h" ]]; then
-    #goes to the help fucnotin that explains how to fix the pip3 error message
-    HELP "$1"
-fi
-
-# Function to install package using pip
+# Function to install pip package
 install_pip_package() {
     package_name="$1"
-    
-    # Attempt to install the package
     python3 -m pip install --user --upgrade "${package_name}" --break-system-packages
-    
-    # Check the exit code of the installation
     if [ $? -eq 0 ]; then
-        # Verify the package installation by trying to import it in Python
-        python3 -c "import ${package_name}" 2>/dev/null
-        
-        if [ $? -eq 0 ]; then
-            echo -e "[ ${BRIGHT}${GREEN}OK${NC} ] ${package_name} installed and verified successfully."
+        if python3 -c "import ${package_name}" &>/dev/null; then
+            echo -e "[ ${GREEN}OK${NC} ] ${package_name} installed and verified successfully."
         else
-            echo -e "[ ${BRIGHT}${RED}FAIL${NC} ] ${package_name} installed but could not be imported in Python."
+            echo -e "[ ${RED}FAIL${NC} ] ${package_name} installed but could not be imported in Python."
             exit 1
         fi
     else
-        echo -e "[ ${BRIGHT}${RED}FAIL${NC} ] Failed to install ${package_name}."
+        echo -e "[ ${RED}FAIL${NC} ] Failed to install ${package_name}."
         exit 1
     fi
 }
 
 # Install packages function
 installPackages() {
-    # Check if the user is root
     if [ "$(id -u)" -eq 0 ]; then
-        # Gives the user something to read so they understand why they got the error
         echo "+++++++++++++++++++++++++++++++++++++++++"
         echo "+   Don't use sudo for this script.     +"
         echo "+   Because it can damage your computer +"
         echo "+++++++++++++++++++++++++++++++++++++++++"
-        echo ""
         exit 1
-    
-    
-
-    # If the user is not root, exit the script
     else
-        # Check if the OS is Linux
-        if [[ "${OSTYPE}" == "${OS}"* ]]; then
-
-            # Install BREW packages
-            echo ""
+        if [[ "${OSTYPE}" == "darwin"* ]]; then
             echo "_________BREW PACKAGES INSTALLATION________"
             for package in "${Packages[@]}"; do
                 install_brew_package "${package}"
             done
 
-            # Install PIP packages
-            echo ""
             echo "_________PIP PACKAGES INSTALLATION________"
             for PIP in "${pipPackages[@]}"; do
                 install_pip_package "${PIP}"
             done
 
-            # Update PIP
-            echo ""
-            echo "_________PIP UPDATES________"
+            echo "_________PIP UPGRADES________"
             upgrade_pip
 
-            # Check if the packages are installed
-            echo ""
             echo "_________INSTALLED PACKAGES________"
             checkForPackages
-        
         else
-            echo -e "[ ${RED}FAIL${NC} ] Wrong OS, please use the correct OS."
+            echo -e "[ ${RED}FAIL${NC} ] Unsupported OS. Please use macOS."
         fi
     fi
 }
 
+# Handle Ctrl+Z (SIGTSTP)
+trap 'EXIT_PROGRAM_WITH_CTRL_Z' SIGTSTP
+
+# Handle Ctrl+C (SIGINT)
+trap 'EXIT_PROGRAM_WITH_CTRL_C' SIGINT
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    HELP
+fi
 
 # Call EthernetConnection function
 EthernetConnection
-
-# Call InstallHomeBrew function
-InstallHomeBrew
