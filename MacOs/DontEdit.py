d1425bf76f96180ac801e2805a112cf3bd34852c
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-09-28 18:17:23 -0400

    Update DontEdit.py

--- MacOs/DontEdit.py
+++ MacOs/DontEdit.py
@@ -67,7 +67,7 @@ LocalManualArgument = {"--MANUAL-LOCAL", "--Manual-Local", "--manual-local", "--
 installRequirement = {"--INSTALL", "--install", "--in"}
 uninstallRequirement = {"--UNINSTALL", "--uninstall", "--un"}
 conenctRDP = {"--xfreerdp", "--XFREERDP", "--XFREE", "--xfree"}
-pipForce = {"--pipForce"}
+pipForce = {"--pipForce","--pip-force","--PIP-FORCE","--pipforce"}
 FIX = {"--FIX", "--fix", "-f", "-F"}
 
 # Define list of scripts to run
