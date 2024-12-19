5a4e7ac926e6391235bef44568fc0e75fab0e0ec
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-12-19 17:01:27 -0500

    Update showOrHideFolders.sh

--- etc/showOrHideFolders.sh
+++ etc/showOrHideFolders.sh
@@ -7,50 +7,42 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
-
 # Define a function to show or hide folders based on user input.
 show_hide() {
     local input="$1"  # Store the user input in a local variable.
     local found=false  # Create a flag to track if a valid option is found.
-
-    # Check if the input matches any option in the "show" array.
-    for option in "${show[@]}"; do
-        if [[ "$option" == "$input" ]]; then
+    if [[ "$1" == "--show" ]]; then
+        # Check if the input matches any option in the "show" array.
+        for option in "${show[@]}"; do
             # If a match is found, set Finder to show hidden files and restart it.
             defaults write com.apple.finder AppleShowAllFiles -bool true
             killall Finder
             found=true
             echo -e "${GREEN}[+]${RESET} Showing Files..."
-
             break
-        fi
-    done
-
-    # If no match is found in the "show" array, check the "hide" array.
-    if [[ "$found" == false ]]; then
-        for option in "${hide[@]}"; do
-            if [[ "$option" == "$input" ]]; then
+        done
+    
+    elif [[ "$1" == "--hide" ]]; then
+        # If no match is found in the "show" array, check the "hide" array.
+        if [[ "${found}" == false ]]; then
+            for option in "${hide[@]}"; do
                 # If a match is found, set Finder to hide hidden files and restart it.
                 defaults write com.apple.finder AppleShowAllFiles -bool false
                 killall Finder
                 found=true
                 echo -e "${GREEN}[+]${RESET} Hidding Files..."
                 break
-            fi
-        done
-    fi
-
-    # If the input is neither in the "show" nor "hide" array, display "Unknown option".
-    if [[ "$found" == false ]]; then
-        echo -e "${RED}[-]${RESET} Unknown option"
-        sleep 1
-        read -p "Do you want to show or hide folders: " new_input
-        show_hide "${new_input}"  # Call the function recursively with the new input.
+            done
+        fi
+    else
+        # If the input is neither in the "show" nor "hide" array, display "Unknown option".
+        if [[ "${found}" == false ]]; then
+            echo -e "${RED}[-]${RESET} Unknown option"
+            sleep 1
+            read -p "Do you want to show or hide folders: " new_input
+            show_hide "${new_input}"  # Call the function recursively with the new input.
+        fi
     fi
 }
-
-# Ask the user for input.
-read -p "Do you want to show or hide folders: " ShowOrHide
-
 # Call the show_hide function with the user input.
-show_hide "${ShowOrHide}"
+show_hide "$1"
