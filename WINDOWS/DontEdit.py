8eb62c81a6022670e5673ccddf2dd93c2ce29188
Will Segale <39059459+WilSegale@users.noreply.github.com>
2025-01-26 09:52:09 -0500

    update on DontEdit for windows

--- WINDOWS/DontEdit.py
+++ WINDOWS/DontEdit.py
@@ -7,3 +7,13 @@ from paramiko import SSHClient, AutoAddPolicy
 import re
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
+# Define color codes for console output
+BRIGHT = '\033[1m'
+GREEN = "\033[92m"
+RED = "\033[91m"
+BLUE = "\033[34m"
+YELLOW = "\033[33m"
+ORANGE_Start = "\033[38;2;255;165;0m"
+GRAY_TEXT = "\033[90m"
+CYAN_TEXT = "\033[36m"
+RESET = "\033[0m"
