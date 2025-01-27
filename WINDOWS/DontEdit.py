9f98b71a2f8f1c3126345a22d7a51f1d323dbc45
Will Segale <39059459+WilSegale@users.noreply.github.com>
2025-01-27 13:35:15 -0500

    update on dontedit file for windows

--- WINDOWS/DontEdit.py
+++ WINDOWS/DontEdit.py
@@ -1,3 +1,15 @@
+import os
+import sys
+import logging
+import socket
+import platform
+import asyncio
+from paramiko import SSHClient, AutoAddPolicy
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from aiohttp import ClientSession, ClientTimeout
+import re
+
+
 # Define color codes for console output
 BRIGHT = '\033[1m'
 GREEN = "\033[92m"
--- WINDOWS/NetBreach.py
+++ WINDOWS/NetBreach.py
@@ -1,14 +1,4 @@
 from DontEdit import *
-import os
-import sys
-import logging
-import socket
-import platform
-import asyncio
-from paramiko import SSHClient, AutoAddPolicy
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from aiohttp import ClientSession, ClientTimeout
-import re
 
 
 # Constants
