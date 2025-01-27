5e1f9ce65d1921d83cacf561f44fc846ba1d94e4
Will Segale <39059459+WilSegale@users.noreply.github.com>
2025-01-27 13:33:07 -0500

    update on windows mode

--- WINDOWS/DontEdit.py
+++ WINDOWS/DontEdit.py
@@ -1,12 +1,3 @@
-import os
-import sys
-import logging
-import socket
-import platform
-from paramiko import SSHClient, AutoAddPolicy
-import re
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
 # Define color codes for console output
 BRIGHT = '\033[1m'
 GREEN = "\033[92m"
--- WINDOWS/NetBreach.py
+++ WINDOWS/NetBreach.py
@@ -1,3 +1,4 @@
+from DontEdit import *
 import os
 import sys
 import logging
@@ -9,16 +10,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from aiohttp import ClientSession, ClientTimeout
 import re
 
-# Define color codes for console output
-BRIGHT = '\033[1m'
-GREEN = "\033[92m"
-RED = "\033[91m"
-BLUE = "\033[34m"
-YELLOW = "\033[33m"
-ORANGE_Start = "\033[38;2;255;165;0m"
-GRAY_TEXT = "\033[90m"
-CYAN_TEXT = "\033[36m"
-RESET = "\033[0m"
 
 # Constants
 LOG_FILE = "netbreach.log"
